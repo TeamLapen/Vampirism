5379931ab718af279d385cd077aa36b7dc80aeb4
Maxanier <maxanier@gmx.de>
2014-08-11 17:00:00 +0200

    Initial Commit

---
+++ src/main/java/de/teamlapen/vampirism/VampirismMod.java
@@ -0,0 +1,10 @@
+package de.teamlapen.vampirism;
+
+import cpw.mods.fml.common.Mod;
+import de.teamlapen.vampirism.util.REFERENCE;
+
+
+@Mod(modid=REFERENCE.MODID,name=REFERENCE.NAME,version=REFERENCE.VERSION)
+public class VampirismMod {
+
+}
---
+++ src/main/java/de/teamlapen/vampirism/util/REFERENCE.java
@@ -0,0 +1,7 @@
+package de.teamlapen.vampirism.util;
+
+public class REFERENCE {
+	public static final String MODID="vampirism";
+	public static final String NAME ="Vampirism";
+	public static final String VERSION="0.1";
+}
