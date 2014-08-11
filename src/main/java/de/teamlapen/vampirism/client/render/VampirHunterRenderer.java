733cf0cd9800a575b3179de25d98583ad9abaeed
Maxanier <maxanier@gmx.de>
2014-08-11 21:39:17 +0200

    Added VampitHunter classes

---
+++ src/main/java/de/teamlapen/vampirism/client/render/VampirHunterRenderer.java
@@ -0,0 +1,21 @@
+package de.teamlapen.vampirism.client.render;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+public class VampirHunterRenderer extends RendererLivingEntity {
+
+	public VampirHunterRenderer(ModelBase p_i1261_1_, float p_i1261_2_) {
+		super(p_i1261_1_, p_i1261_2_);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/de/teamlapen/vampirism/entity/VampirHunter.java
@@ -0,0 +1,13 @@
+package de.teamlapen.vampirism.entity;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class VampirHunter extends EntityMob {
+
+	public VampirHunter(World p_i1738_1_) {
+		super(p_i1738_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
