56990b96d2979ef87d00d956484553276bb1e3ab
Maxanier <maxanier@gmx.de>
2014-08-12 17:11:06 +0200

    Edited Vampirehunter

--- src/main/java/de/teamlapen/vampirism/client/render/VampireHunterRenderer.java
+++ src/main/java/de/teamlapen/vampirism/client/render/VampireHunterRenderer.java
@@ -9,6 +9,8 @@ import net.minecraft.util.ResourceLocation;
 
 @SideOnly(Side.CLIENT)
 public class VampireHunterRenderer extends RendererLivingEntity {
+	
+	private static final ResourceLocation texture = new ResourceLocation("vampirism:texture/entity/vampireHunter.png");
 
 	public VampireHunterRenderer(ModelBase p_i1261_1_, float p_i1261_2_) {
 		super(p_i1261_1_, p_i1261_2_);
@@ -16,8 +18,7 @@ public class VampireHunterRenderer extends RendererLivingEntity {
 
 	@Override
 	protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
-		// TODO Auto-generated method stub
-		return null;
+		return texture;
 	}
 
 }
--- src/main/java/de/teamlapen/vampirism/entity/EntityVampireHunter.java
+++ src/main/java/de/teamlapen/vampirism/entity/EntityVampireHunter.java
@@ -1,7 +1,10 @@
 package de.teamlapen.vampirism.entity;
 
+import de.teamlapen.vampirism.util.MobProperties;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 public class EntityVampireHunter extends BasicMob {
@@ -10,7 +13,20 @@ public class EntityVampireHunter extends BasicMob {
 		super(p_i1738_1_);
 		
 		this.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20.0D);
+		
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this,EntityVampire.class,2*MobProperties.vampireHunter_movementSpeed,false));
+	}
+	
+	protected void applyEntityAttributes(){
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(MobProperties.vampire_maxHealth);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(MobProperties.vampireHunter_attackDamage);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(MobProperties.vampireHunter_movementSpeed);
+	}
+	@Override
+	protected Item getDropItem(){
+		return null;
+		
 	}
 
 }
