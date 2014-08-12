8eacd52e3822e60583881e6d2fccb1d2c50a00cc
Mistadon <moritzwachsmuth@gmail.com>
2014-08-12 17:43:49 +0200

    CommonProxy: allBiomes variable (fixes bug?)

--- src/main/java/de/teamlapen/vampirism/proxy/CommonProxy.java
+++ src/main/java/de/teamlapen/vampirism/proxy/CommonProxy.java
@@ -1,5 +1,8 @@
 package de.teamlapen.vampirism.proxy;
 
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterators;
+
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
 import cpw.mods.fml.common.registry.EntityRegistry;
@@ -11,14 +14,17 @@ public abstract class CommonProxy implements IProxy{
 	public void registerEntitys(){
 		int id=0;
 		
+		BiomeGenBase[] allBiomes = Iterators.toArray(Iterators.filter(Iterators.forArray(BiomeGenBase.getBiomeGenArray()),
+				Predicates.notNull()), BiomeGenBase.class);
+		
 		//Registration of vampire hunter
 		EntityRegistry.registerModEntity(EntityVampireHunter.class, "VampireHunter", id, VampirismMod.instance, 80, 1, true);
-		EntityRegistry.addSpawn(EntityVampireHunter.class, 2, 0, 1, EnumCreatureType.monster, BiomeGenBase.stoneBeach,BiomeGenBase.desert);
+		EntityRegistry.addSpawn(EntityVampireHunter.class, 2, 0, 1, EnumCreatureType.monster, allBiomes);
 		id++;
 		
 		//Registration of vampire
 		EntityRegistry.registerModEntity(EntityVampire.class, "Vampire", id, VampirismMod.instance,80, 1, true);
-		EntityRegistry.addSpawn(EntityVampire.class, 2, 0, 1, EnumCreatureType.monster, BiomeGenBase.stoneBeach,BiomeGenBase.desert);
+		EntityRegistry.addSpawn(EntityVampire.class, 2, 0, 1, EnumCreatureType.monster, allBiomes);
 		id++;
 	}
 
