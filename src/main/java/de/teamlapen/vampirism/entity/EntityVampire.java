e01fd4c63e2d0823cc028c611521650bc1a162b3
Mistadon <moritzwachsmuth@gmail.com>
2014-08-18 19:48:30 +0200

    Changes in EntityVampire

--- src/main/java/de/teamlapen/vampirism/entity/EntityVampire.java
+++ src/main/java/de/teamlapen/vampirism/entity/EntityVampire.java
@@ -18,35 +18,19 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
-
 public class EntityVampire extends EntityZombie {
-	//TODO Sounds
+	// TODO Sounds
 
 	public EntityVampire(World par1World) {
 		super(par1World);
 
 		// Avoids Vampire Hunters TODO Distance (3rd argument)
-		this.tasks.addTask(1, new EntityAIAvoidEntity(this,
+		this.tasks.addTask(5, new EntityAIAvoidEntity(this,
 				EntityVampireHunter.class, 10.0F,
 				MobProperties.vampire_movementSpeed,
 				MobProperties.vampire_movementSpeed * 1.5));
-		// Attacks player
-		this.tasks.addTask(2, new EntityAIAttackOnCollide(this,
-				EntityPlayer.class, MobProperties.vampire_movementSpeed * 2,
-				true));
-		// Attacks villagers
-		this.tasks.addTask(3, new EntityAIAttackOnCollide(this,
-				EntityVillager.class,
-				MobProperties.vampire_movementSpeed * 1.5, true));
-		// Moves through villages
-		this.tasks
-				.addTask(4, new EntityAIMoveThroughVillage(this, 1.0D, false));
-		// Avoids sun
-		this.tasks.addTask(10, new EntityAIFleeSun(this,
-				MobProperties.vampire_movementSpeed * 1.5));
 	}
-	
-	
+
 	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
@@ -58,9 +42,57 @@ public class EntityVampire extends EntityZombie {
 				.setBaseValue(MobProperties.vampire_movementSpeed);
 	}
 
+	public void onKillEntity(EntityLivingBase p_70074_1_) {
+		super.onKillEntity(p_70074_1_);
 
-	
-	//Overrides methods that are not needed, but declared in EntityZombie.class
+		if ((this.worldObj.difficultySetting == EnumDifficulty.NORMAL || this.worldObj.difficultySetting == EnumDifficulty.HARD)
+				&& p_70074_1_ instanceof EntityVillager) {
+			if (this.worldObj.difficultySetting != EnumDifficulty.HARD
+					&& this.rand.nextBoolean()) {
+				return;
+			}
+
+			EntityVampire entityvampire = new EntityVampire(this.worldObj);
+			entityvampire.copyLocationAndAnglesFrom(p_70074_1_);
+			this.worldObj.removeEntity(p_70074_1_);
+			entityvampire.onSpawnWithEgg((IEntityLivingData) null);
+			entityvampire.setVillager(true);
+
+			if (p_70074_1_.isChild()) {
+				entityvampire.setChild(true);
+			}
+
+			this.worldObj.spawnEntityInWorld(entityvampire);
+			this.worldObj.playAuxSFXAtEntity((EntityPlayer) null, 1016,
+					(int) this.posX, (int) this.posY, (int) this.posZ, 0);
+		}
+	}
+
+	//Sets it on fire in light, kills it in sunlight
+	public void onLivingUpdate() {
+		float brightness = this.getBrightness(1.0F);
+		boolean canSeeSky = this.worldObj.canBlockSeeTheSky(
+				MathHelper.floor_double(this.posX),
+				MathHelper.floor_double(this.posY),
+				MathHelper.floor_double(this.posZ));
+		if(!this.worldObj.isRemote) {
+			if (this.worldObj.isDaytime()) {
+				if (brightness > 0.5F
+						&& this.rand.nextFloat() * 30.0F < (brightness - 0.4F) * 2.0F
+						&& canSeeSky) {
+					this.setDead();
+				}
+			} else {
+				if (brightness > 0.5F
+						&& this.rand.nextFloat() * 30.0F < (brightness - 0.4F) * 2.0F) {
+					this.setFire(8);
+				}
+			}
+		}
+	}
+
+	// Overrides methods that are not needed, but declared in EntityZombie.class
 	@Override
-	protected void convertToVillager() {}
+	protected void convertToVillager() {
+	}
 }
