4e93efae69a9247e2db3a820c658f0d080a81248
Mistadon <moritzwachsmuth@gmail.com>
2014-08-18 21:32:05 +0200

    -debuggged vampire texture -debugged vampire -made vampires burn in light and die in sunlight

--- src/main/java/de/teamlapen/vampirism/entity/EntityVampire.java
+++ src/main/java/de/teamlapen/vampirism/entity/EntityVampire.java
@@ -1,6 +1,7 @@
 package de.teamlapen.vampirism.entity;
 
 import de.teamlapen.vampirism.util.MobProperties;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
@@ -8,7 +9,11 @@ import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import net.minecraft.entity.ai.EntityAIFleeSun;
 import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -18,17 +23,39 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
-public class EntityVampire extends EntityZombie {
+public class EntityVampire extends BasicMob {
 	// TODO Sounds
 
 	public EntityVampire(World par1World) {
 		super(par1World);
 
+		// Attack player
+		this.tasks.addTask(1, new EntityAIAttackOnCollide(this,
+				EntityPlayer.class, 1.0D, false));
+		// Attack vampire hunter
+		this.tasks.addTask(1, new EntityAIAttackOnCollide(this,
+				EntityVampireHunter.class, 1.0D, true));
+		// Attack villager
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this,
+				EntityVillager.class, 1.0D, true));
+		// Search for players
+		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this,
+				EntityPlayer.class, 0, true));
+		// Search for vampire hunters
+		this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this,
+				EntityVampireHunter.class, 0, true));
+		// Search for villagers
+		this.targetTasks.addTask(4, new EntityAINearestAttackableTarget(this,
+				EntityVillager.class, 0, true));
+		this.tasks.addTask(4, new EntityAIMoveTowardsRestriction(this, 1.0D));
 		// Avoids Vampire Hunters TODO Distance (3rd argument)
 		this.tasks.addTask(5, new EntityAIAvoidEntity(this,
 				EntityVampireHunter.class, 10.0F,
 				MobProperties.vampire_movementSpeed,
 				MobProperties.vampire_movementSpeed * 1.5));
+		this.tasks
+				.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+
 	}
 
 	@Override
@@ -42,57 +69,42 @@ public class EntityVampire extends EntityZombie {
 				.setBaseValue(MobProperties.vampire_movementSpeed);
 	}
 
-	public void onKillEntity(EntityLivingBase p_70074_1_) {
-		super.onKillEntity(p_70074_1_);
+//	public void onKillEntity(EntityLivingBase p_70074_1_) {
+//		super.onKillEntity(p_70074_1_);
+//
+//		if ((this.worldObj.difficultySetting == EnumDifficulty.NORMAL || this.worldObj.difficultySetting == EnumDifficulty.HARD)
+//				&& p_70074_1_ instanceof EntityVillager) {
+//			if (this.worldObj.difficultySetting != EnumDifficulty.HARD
+//					&& this.rand.nextBoolean()) {
+//				return;
+//			}
+//
+//			EntityVampire entityvampire = new EntityVampire(this.worldObj);
+//			entityvampire.copyLocationAndAnglesFrom(p_70074_1_);
+//			this.worldObj.removeEntity(p_70074_1_);
+//			entityvampire.onSpawnWithEgg((IEntityLivingData) null);
+//
+//			this.worldObj.spawnEntityInWorld(entityvampire);
+//			this.worldObj.playAuxSFXAtEntity((EntityPlayer) null, 1016,
+//					(int) this.posX, (int) this.posY, (int) this.posZ, 0);
+//		}
+//	}
 
-		if ((this.worldObj.difficultySetting == EnumDifficulty.NORMAL || this.worldObj.difficultySetting == EnumDifficulty.HARD)
-				&& p_70074_1_ instanceof EntityVillager) {
-			if (this.worldObj.difficultySetting != EnumDifficulty.HARD
-					&& this.rand.nextBoolean()) {
-				return;
-			}
-
-			EntityVampire entityvampire = new EntityVampire(this.worldObj);
-			entityvampire.copyLocationAndAnglesFrom(p_70074_1_);
-			this.worldObj.removeEntity(p_70074_1_);
-			entityvampire.onSpawnWithEgg((IEntityLivingData) null);
-			entityvampire.setVillager(true);
-
-			if (p_70074_1_.isChild()) {
-				entityvampire.setChild(true);
-			}
-
-			this.worldObj.spawnEntityInWorld(entityvampire);
-			this.worldObj.playAuxSFXAtEntity((EntityPlayer) null, 1016,
-					(int) this.posX, (int) this.posY, (int) this.posZ, 0);
-		}
-	}
-
-	//Sets it on fire in light, kills it in sunlight
 	public void onLivingUpdate() {
-		float brightness = this.getBrightness(1.0F);
-		boolean canSeeSky = this.worldObj.canBlockSeeTheSky(
-				MathHelper.floor_double(this.posX),
-				MathHelper.floor_double(this.posY),
-				MathHelper.floor_double(this.posZ));
-		if(!this.worldObj.isRemote) {
-			if (this.worldObj.isDaytime()) {
-				if (brightness > 0.5F
-						&& this.rand.nextFloat() * 30.0F < (brightness - 0.4F) * 2.0F
-						&& canSeeSky) {
+		if (!this.worldObj.isRemote) {
+			float brightness = this.getBrightness(1.0F);
+			boolean canSeeSky = this.worldObj.canBlockSeeTheSky(
+					MathHelper.floor_double(this.posX),
+					MathHelper.floor_double(this.posY),
+					MathHelper.floor_double(this.posZ));
+			if (brightness > 0.5F) {
+				if (this.worldObj.isDaytime() && canSeeSky) {
 					this.setDead();
-				}
-			} else {
-				if (brightness > 0.5F
-						&& this.rand.nextFloat() * 30.0F < (brightness - 0.4F) * 2.0F) {
-					this.setFire(8);
+				} else {
+					this.setFire(2);
 				}
 			}
 		}
-	}
-
-	// Overrides methods that are not needed, but declared in EntityZombie.class
-	@Override
-	protected void convertToVillager() {
+		super.onLivingUpdate();
 	}
 }
