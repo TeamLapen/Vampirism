89125e93c94beb937b49ac46d819b9b4a08f2fcd
Mistadon <moritzwachsmuth@gmail.com>
2014-08-25 18:21:49 +0200

    Changes in RendererBloodAltar

--- src/main/java/de/teamlapen/vampirism/client/render/RendererBloodAltar.java
+++ src/main/java/de/teamlapen/vampirism/client/render/RendererBloodAltar.java
@@ -19,9 +19,11 @@ public class RendererBloodAltar extends TileEntitySpecialRenderer {
 	
         //The model of your block
         private final ModelBloodAltar model;
+        private final ResourceLocation texture;
         
         public RendererBloodAltar() {
                 this.model = new ModelBloodAltar();
+                texture = new ResourceLocation(REFERENCE.MODID +":textures/blocks/bloodAltar.png");
         }
         
         private void adjustRotatePivotViaMeta(World world, int x, int y, int z) {
@@ -37,19 +39,14 @@ public class RendererBloodAltar extends TileEntitySpecialRenderer {
                 GL11.glPushMatrix();
         //This is setting the initial location.
                 GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-        //This is the texture of your block. It's pathed to be the same place as your other blocks here.
-                //Outdated bindTextureByName("/mods/roads/textures/blocks/TrafficLightPoleRed.png");
-       //Use in 1.6.2  this
-                ResourceLocation textures = (new ResourceLocation(REFERENCE.MODID + ":textures/block/bloodAltar.png")); 
-        //the ':' is very important
         //binding the textures
-                Minecraft.getMinecraft().renderEngine.bindTexture(textures);
+                this.bindTexture(texture);
 
         //This rotation part is very important! Without it, your model will render upside-down! And for some reason you DO need PushMatrix again!                       
                 GL11.glPushMatrix();
                 GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
         //A reference to your Model file. Again, very important.
-                this.model.render((Entity)null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+                model.render(null, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
         //Tell it to stop rendering for both the PushMatrix's
                 GL11.glPopMatrix();
                 GL11.glPopMatrix();
