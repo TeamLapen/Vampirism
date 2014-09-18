140b8ec1717ceacf1576e1bbd1587e2faa52157d
Maxanier <maxanier@gmx.de>
2014-09-18 21:11:26 +0200

    Texturefixes

--- src/main/java/de/teamlapen/vampirism/client/render/vanilla/RenderVampireZombie.java
+++ src/main/java/de/teamlapen/vampirism/client/render/vanilla/RenderVampireZombie.java
@@ -5,10 +5,11 @@ import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.util.ResourceLocation;
 import de.teamlapen.vampirism.entity.VampireMob;
+import de.teamlapen.vampirism.util.REFERENCE;
 
 public class RenderVampireZombie extends RenderZombie {
 
-	private static final ResourceLocation vampireZombiePigmanTextures = new ResourceLocation("textures/entity/cow/cow.png");
+	private static final ResourceLocation vampireZombiePigmanTextures = new ResourceLocation(REFERENCE.MODID + ":textures/entity/vanilla/vampirePigman.png");
 	private static final ResourceLocation vampireZombieTextures = new ResourceLocation("textures/entity/pig/pig.png");
 	private static final ResourceLocation vampireZombieVillagerTextures = new ResourceLocation("textures/entity/pig/pig.png");
 
