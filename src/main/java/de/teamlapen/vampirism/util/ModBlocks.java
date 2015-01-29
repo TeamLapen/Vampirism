a26f8685eddaa2df2da5659f3fdee4bccfee81d5
Maxanier <maxanier@gmx.de>
2015-01-29 11:21:08 +0100

    Added recipe for BloodAltarTier2

--- src/main/java/de/teamlapen/vampirism/util/ModBlocks.java
+++ src/main/java/de/teamlapen/vampirism/util/ModBlocks.java
@@ -2,7 +2,11 @@ package de.teamlapen.vampirism.util;
 
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.MaterialLiquid;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 import de.teamlapen.vampirism.block.BlockBloodAltar;
 import de.teamlapen.vampirism.block.BlockBloodAltarTier2;
@@ -22,5 +26,7 @@ public class ModBlocks {
 		GameRegistry.registerBlock(bloodAltarTier2, ItemBlock.class,BlockBloodAltarTier2.name);
 		GameRegistry.registerTileEntity(TileEntityBloodAltar.class, "TileEntityBloodAltar");
 		GameRegistry.registerTileEntity(TileEntityBloodAltarTier2.class, "TileEntityBloodAltarTier2");
+		
+		GameRegistry.addRecipe(new ItemStack(bloodAltarTier2,1), " X ","XYX","ZZZ",'X',Blocks.glass,'Y',Items.gold_ingot,'Z',Items.iron_ingot);
 	}
 }
