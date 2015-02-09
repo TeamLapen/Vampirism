7f422e3fd50e5417af7b6c3e350bebff8fc9cf78
Maxanier <maxanier@gmx.de>
2015-02-09 21:22:20 +0100

    Fixes another really strange error

--- src/main/java/de/teamlapen/vampirism/network/SpawnCustomParticlePacket.java
+++ src/main/java/de/teamlapen/vampirism/network/SpawnCustomParticlePacket.java
@@ -8,6 +8,8 @@ import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import de.teamlapen.vampirism.client.render.particle.FlyingBloodParticle;
 import de.teamlapen.vampirism.util.Logger;
 
@@ -23,14 +25,13 @@ public class SpawnCustomParticlePacket implements IMessage {
 
 		@Override
 		public IMessage onMessage(SpawnCustomParticlePacket message, MessageContext ctx) {
-
 			try {
 				switch (message.data.getInteger("type")) {
 				case 0:
 					for (int i = 0; i < message.amount; i++) {
-						EntityFX p = new FlyingBloodParticle(Minecraft.getMinecraft().theWorld, message.posX, message.posY, message.posZ,
-								message.data);
-						Minecraft.getMinecraft().effectRenderer.addEffect(p);
+						
+						FlyingBloodParticle.addParticle(new FlyingBloodParticle(message.posX, message.posY, message.posZ,
+								message.data));
 
 					}
 					break;
