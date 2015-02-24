ec1d5da7743c5c52418959ce89f1a73b52f60643
Maxanier <maxanier@gmx.de>
2015-02-24 19:53:15 +0100

    Made Vampirehunter target attacking vampires over bigger distance and also target attacking zombies nearby

--- src/main/java/de/teamlapen/vampirism/entity/EntityAIDefendVillage.java
+++ src/main/java/de/teamlapen/vampirism/entity/EntityAIDefendVillage.java
@@ -2,7 +2,11 @@ package de.teamlapen.vampirism.entity;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAITarget;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.PathEntity;
+import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.util.MathHelper;
 import net.minecraft.village.Village;
 import de.teamlapen.vampirism.entity.player.VampirePlayer;
 import de.teamlapen.vampirism.util.BALANCE;
@@ -18,7 +22,7 @@ public class EntityAIDefendVillage extends EntityAITarget {
 	EntityLivingBase villageAgressorTarget;
 
 	public EntityAIDefendVillage(EntityVampireHunter h) {
-		super(h, false, true);
+		super(h, false, false);
 		hunter = h;
 		this.setMutexBits(1);
 	}
@@ -30,13 +34,36 @@ public class EntityAIDefendVillage extends EntityAITarget {
 			return false;
 
 		this.villageAgressorTarget = v.findNearestVillageAggressor(hunter);
-
 		if (this.isSuitableTarget(villageAgressorTarget, false)) {
 			if (villageAgressorTarget instanceof EntityPlayer) {
 				if (VampirePlayer.get((EntityPlayer) (villageAgressorTarget)).getLevel() > BALANCE.VAMPIRE_HUNTER_ATTACK_LEVEL) {
 					return true;
 				}
-			} else if (villageAgressorTarget instanceof EntityVampire) {
+			} else if(villageAgressorTarget instanceof EntityZombie){
+				//Only attack Zombies if they can easily be reached, see EntityAITarget#canEasilyReach 
+				PathEntity pathentity = this.taskOwner.getNavigator().getPathToEntityLiving(villageAgressorTarget);
+
+		        if (pathentity == null)
+		        {
+		            return false;
+		        }
+		        else
+		        {
+		            PathPoint pathpoint = pathentity.getFinalPathPoint();
+
+		            if (pathpoint == null)
+		            {
+		                return false;
+		            }
+		            else
+		            {
+		                int i = pathpoint.xCoord - MathHelper.floor_double(villageAgressorTarget.posX);
+		                int j = pathpoint.zCoord - MathHelper.floor_double(villageAgressorTarget.posZ);
+		                return (double)(i * i + j * j) <= 2.25D;
+		            }
+		        }
+			}
+			else if (villageAgressorTarget instanceof EntityVampire) {
 				return true;
 			}
 		}
