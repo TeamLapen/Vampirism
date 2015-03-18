3cad40cb1b671fa7362fc8a9358a9a2da3af1060
wildbill22 <billwerth2@msn.com>
2015-03-17 21:29:16 -0700

    Fixed #18
    Kind of a kludge, as I don't completely understand why it was failing.

--- src/main/java/de/teamlapen/vampirism/block/BlockFlower/BasicFlower.java
+++ src/main/java/de/teamlapen/vampirism/block/BlockFlower/BasicFlower.java
@@ -1,17 +1,22 @@
 package de.teamlapen.vampirism.block.BlockFlower;
 
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import de.teamlapen.vampirism.VampirismMod;
 import de.teamlapen.vampirism.util.REFERENCE;
 
 public abstract class BasicFlower extends BlockFlower {
 	@SideOnly(Side.CLIENT)
-    protected IIcon iIcon;
+    protected IIcon[] iIcon;
 
 	protected BasicFlower(String name, String textureName) {
 		super(getUniqueFlowerID()); // Unique ID
@@ -19,6 +24,7 @@ public abstract class BasicFlower extends BlockFlower {
 		this.setBlockName(name);
 		this.setStepSound(soundTypeGrass);
 		this.setHardness(0.0F);
+		iIcon = new IIcon[1];
 	}
 	
 	// The existing flowers are created in Block class (normally just 2)
@@ -43,10 +49,25 @@ public abstract class BasicFlower extends BlockFlower {
 		return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
 	}
 	
-	@Override
+    /**
+     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+     */
+	// Added to prevent a crash opening creative tab, issue #18
+    @Override
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item p_149666_1_, CreativeTabs p_149666_2_, List p_149666_3_)
+    {
+        for (int i = 0; i < this.iIcon.length; ++i)
+        {
+            p_149666_3_.add(new ItemStack(p_149666_1_, 1, i));
+        }
+    }
+
+    @Override
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister iconRegister) {
-		iIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+		iIcon[0] = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
 	}
 	
     /**
@@ -55,6 +76,6 @@ public abstract class BasicFlower extends BlockFlower {
 	@Override
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int p_149691_1_, int p_149691_2_) {
-        return this.iIcon;
+        return this.iIcon[0];
     }
 }
