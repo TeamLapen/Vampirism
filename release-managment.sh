0b81ab96486db8a83cdce06e2953021da85b1550
maxanier <maxanier@gmx.de>
2015-07-23 18:37:08 +0200

    Redid build scripts. Attempt 7 #release

--- release-managment.sh
+++ release-managment.sh
@@ -24,7 +24,8 @@ fi
 #./gradlew build curse
 version=$(<version.txt)
 echo "Finished building version: " $version
-if [$(printenv RELEASE)="true"]; then
+if ["$(printenv RELEASE)"="true"]
+then
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $version $(printenv GIT_COMMIT) $version $version)
 token=$(printenv TOKEN)
 curl --data "$API_JSON" https://api.github.com/repos/${1}/${2}/releases?access_token=${token}
