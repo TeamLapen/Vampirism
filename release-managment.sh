fcd7ab564b3a61512a53a10ef65fb05f509f1e69
Maxanier <maxanier@gmx.de>
2015-08-09 12:00:14 +0200

    Trying to solve missing tag error #build

--- release-managment.sh
+++ release-managment.sh
@@ -2,6 +2,8 @@
 echo ""
 echo ""
 echo "VersionManagment:"
+git fetch -t
+git show-ref --tags
 #Get commit message
 commsg=$(git show -s --format=%s $(printenv GIT_COMMIT))
 echo "Commit message: " $commsg
@@ -24,9 +26,9 @@ fi
 ./gradlew build
 version=$(<version.txt)
 echo "Finished building version: " $version
+token=$(printenv TOKEN)
 if [[ $commsg = *"$r"* ]]; then
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $version $(printenv GIT_COMMIT) $version $version)
-token=$(printenv TOKEN)
 curl -u maxanier:${token} --data "$API_JSON" https://api.github.com/repos/${1}/${2}/releases
 fi
 API_JSON=$(printf '{ "body":"[DRONE]%s"}' $(printenv DRONE_BUILD_URL))
