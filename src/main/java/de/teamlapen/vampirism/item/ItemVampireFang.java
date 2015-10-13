62ab0e2840cf357b623c0f2c7f9a9d0e9869ac67
Maxanier <maxanier@gmx.de>
2015-10-13 13:00:29 +0200

    Tried to fix a really strange bug

--- src/main/java/de/teamlapen/vampirism/item/ItemVampireFang.java
+++ src/main/java/de/teamlapen/vampirism/item/ItemVampireFang.java
@@ -1,13 +1,13 @@
 package de.teamlapen.vampirism.item;
 
+import de.teamlapen.vampirism.ModPotion;
+import de.teamlapen.vampirism.entity.player.VampirePlayer;
+import de.teamlapen.vampirism.util.BALANCE;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
-import de.teamlapen.vampirism.ModPotion;
-import de.teamlapen.vampirism.entity.player.VampirePlayer;
-import de.teamlapen.vampirism.util.BALANCE;
 
 public class ItemVampireFang extends BasicItem {
 
@@ -17,10 +17,19 @@ public class ItemVampireFang extends BasicItem {
 		super(NAME);
 	}
 
+	protected ItemVampireFang(String name) {
+		super(name);
+	}
+
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
 		if (!player.isPotionActive(ModPotion.sanguinare) && VampirePlayer.get(player).getLevel() == 0) {
-			player.addPotionEffect(new PotionEffect(ModPotion.sanguinare.id, BALANCE.VAMPIRE_PLAYER_SANGUINARE_DURATION * 20));
+			if (ModPotion.sanguinare.id < 0) {
+				//This should not be able to happen, but I have seen a crash where it seems so
+				VampirePlayer.get(player).levelUp();
+			} else {
+				player.addPotionEffect(new PotionEffect(ModPotion.sanguinare.id, BALANCE.VAMPIRE_PLAYER_SANGUINARE_DURATION * 20));
+			}
 			player.addPotionEffect(new PotionEffect(Potion.poison.id, 60));
 		}
 		return stack;
--- src/main/java/de/teamlapen/vampirism/item/ItemWeakVampireFang.java
+++ src/main/java/de/teamlapen/vampirism/item/ItemWeakVampireFang.java
@@ -1,16 +1,11 @@
 package de.teamlapen.vampirism.item;
 
 import de.teamlapen.vampirism.Configs;
-import de.teamlapen.vampirism.ModPotion;
-import de.teamlapen.vampirism.entity.player.VampirePlayer;
-import de.teamlapen.vampirism.util.BALANCE;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-public class ItemWeakVampireFang extends BasicItem {
+public class ItemWeakVampireFang extends ItemVampireFang {
     public static final String name = "weakVampireFang";
 
     public ItemWeakVampireFang() {
@@ -20,10 +15,6 @@ public class ItemWeakVampireFang extends BasicItem {
     @Override
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
         if (Configs.realismMode) return stack;//Only strong fangs can turn
-        if (!player.isPotionActive(ModPotion.sanguinare) && VampirePlayer.get(player).getLevel() == 0) {
-            player.addPotionEffect(new PotionEffect(ModPotion.sanguinare.id, BALANCE.VAMPIRE_PLAYER_SANGUINARE_DURATION * 20));
-            player.addPotionEffect(new PotionEffect(Potion.poison.id, 60));
-        }
-        return stack;
+        return super.onItemRightClick(stack, world, player);
     }
 }
