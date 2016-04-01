4ae3f2af660b5a2e2f94a22441e83461cd8496e6
Maxanier <maxanier@gmx.de>
2016-04-01 18:08:14 +0200

    Minor fix

--- src/main/java/de/teamlapen/vampirism/entity/factions/PlayableFaction.java
+++ src/main/java/de/teamlapen/vampirism/entity/factions/PlayableFaction.java
@@ -13,8 +13,8 @@ import net.minecraftforge.common.capabilities.Capability;
 public class PlayableFaction<T extends IFactionPlayer> extends Faction<T> implements IPlayableFaction<T> {
     private final int highestLevel;
     private final Capability<T> playerCapability;
-    private boolean renderLevel = true;
     private final ResourceLocation key;
+    private boolean renderLevel = true;
 
     PlayableFaction(String name, Class<T> entityInterface, int color, ResourceLocation key, Capability<T> playerCapability, int highestLevel) {
         super(name, entityInterface, color);
@@ -29,18 +29,13 @@ public class PlayableFaction<T extends IFactionPlayer> extends Faction<T> implem
     }
 
     @Override
-    public T getPlayerCapability(EntityPlayer player) {
-        return player.getCapability(playerCapability, null);
-    }
-
-    @Override
-    public T getPlayerCapability(EntityPlayer player) {
-        return player.getCapability(playerCapability, null);
-    }
-
-    @Override
     public ResourceLocation getKey() {
         return key;
+    }
+
+    @Override
+    public T getPlayerCapability(EntityPlayer player) {
+        return player.getCapability(playerCapability, null);
     }
 
     @Override
