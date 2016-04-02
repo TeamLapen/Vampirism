cc8ecea9baf9b6ca0941aa74a7ce70d3b7e62abf
Maxanier <maxanier@gmx.de>
2016-04-03 00:19:37 +0200

    Updated resources

--- src/main/java/de/teamlapen/vampirism/blocks/BlockMedChair.java
+++ src/main/java/de/teamlapen/vampirism/blocks/BlockMedChair.java
@@ -12,6 +12,7 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
@@ -35,8 +36,17 @@ public class BlockMedChair extends VampirismBlock {
     }
 
     @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(PART, EnumPart.fromMeta(meta >> 2)).withProperty(FACING, EnumFacing.getHorizontal(meta));
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        super.breakBlock(worldIn, pos, state);
+        EnumFacing dir = state.getValue(FACING);
+        BlockPos other;
+        if (state.getValue(PART) == EnumPart.TOP) {
+            other = pos.offset(dir);
+            worldIn.spawnEntityInWorld(new EntityItem(worldIn, pos.getX(), pos.getY() + 1, pos.getZ(), new ItemStack(ModItems.itemMedChair, 1)));
+        } else {
+            other = pos.offset(dir.getOpposite());
+        }
+        worldIn.setBlockToAir(other);
     }
 
     @Override
@@ -45,13 +55,8 @@ public class BlockMedChair extends VampirismBlock {
     }
 
     @Override
-    protected BlockState createBlockState() {
-        return new BlockState(this, FACING, PART);
-    }
-
-    @Override
-    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer).withProperty(FACING, placer.getHorizontalFacing().rotateY().rotateY()).withProperty(PART, EnumPart.fromMeta(placer.getRNG().nextInt(2)));//TODO
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(PART, EnumPart.fromMeta(meta >> 2)).withProperty(FACING, EnumFacing.getHorizontal(meta));
     }
 
     @Override
@@ -92,9 +97,25 @@ public class BlockMedChair extends VampirismBlock {
         return super.onBlockActivated(worldIn, pos, state, playerIn, side, hitX, hitY, hitZ);
     }
 
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer).withProperty(FACING, placer.getHorizontalFacing().rotateY().rotateY()).withProperty(PART, EnumPart.fromMeta(placer.getRNG().nextInt(2)));//TODO
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, FACING, PART);
+    }
+
     public enum EnumPart implements IStringSerializable {
         TOP("top", 0), BOTTOM("bottom", 1);
 
+        public static EnumPart fromMeta(int meta) {
+            if (meta == 1) {
+                return BOTTOM;
+            }
+            return TOP;
+        }
         public final String name;
         public final int meta;
 
@@ -103,20 +124,13 @@ public class BlockMedChair extends VampirismBlock {
             this.meta = meta;
         }
 
-        public static EnumPart fromMeta(int meta) {
-            if (meta == 1) {
-                return BOTTOM;
-            }
-            return TOP;
+        public int getMeta() {
+            return meta;
         }
 
         @Override
         public String getName() {
             return name;
-        }
-
-        public int getMeta() {
-            return meta;
         }
     }
 }
