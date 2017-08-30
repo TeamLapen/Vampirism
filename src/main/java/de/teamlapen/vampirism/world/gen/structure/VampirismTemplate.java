da551beba6606f73fb0d93739a8231bac1dce53a
Max Becker - maxanier <maxanier@maxgb.de>
2017-08-30 20:31:49 +0100

    Preparing for #224

---
+++ src/main/java/de/teamlapen/vampirism/world/gen/structure/VampirismTemplate.java
@@ -0,0 +1,15 @@
+package de.teamlapen.vampirism.world.gen.structure;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.gen.structure.template.Template;
+
+public class VampirismTemplate extends Template {
+
+
+    @Override
+    public void read(NBTTagCompound compound) {
+        super.read(compound);
+
+
+    }
+}
