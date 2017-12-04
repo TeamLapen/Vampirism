4d15fc5d4d7660bd8b4f1aaddc9a4cb28a9f2147
maxanier <dev@maxgb.de>
2017-12-04 21:59:37 +0000

    Fix to 6381c20d0cd7c945c4336cc00ed21950b2d343b8. Compile error to mapping difference

--- src/main/java/de/teamlapen/vampirism/world/gen/WorldGenHunterCamp.java
+++ src/main/java/de/teamlapen/vampirism/world/gen/WorldGenHunterCamp.java
@@ -96,7 +96,7 @@ public class WorldGenHunterCamp extends WorldGenerator {
                 EntityAdvancedHunter hunter = new EntityAdvancedHunter(worldIn);
                 AxisAlignedBB box = new AxisAlignedBB(center.add(-7, 0, -7), center.add(7, 0, 7));
                 UtilLib.spawnEntityInWorld(worldIn, box, hunter, 5);
-                hunter.setCampArea(box.grow(3, 5, 3));
+                hunter.setCampArea(box.expand(3, 5, 3));
                 if (VampirismWorldGen.debug)
                     VampirismMod.log.i("HunterCamp", "Generated advanced hunter camp at %s", center);
                 return true;
