d4d8d6eac03c0fd8c2c371c4ffae40f2aa8ed5cc
maxanier <dev@maxgb.de>
2017-12-30 11:55:37 +0000

    Added blood values by iamtpage for Twilight Forest and Sophisticated values. Closes #250

--- src/main/java/de/teamlapen/vampirism/config/BloodValueLoader.java
+++ src/main/java/de/teamlapen/vampirism/config/BloodValueLoader.java
@@ -56,6 +56,8 @@ public class BloodValueLoader {
 
         loadBloodValuesModCompat("animania");
         loadBloodValuesModCompat("ancientwarfarenpc");
+        loadBloodValuesModCompat("twilightforest");
+        loadBloodValuesModCompat("sophisticatedwolves");
     }
 
 
