bb8e1555befc1c44fbd026cc7dce4b7ee33be8ce
max <maxanier@maxgb.de>
2018-04-13 16:32:47 +0200

    Make hunter and vampire implement IMob. Testing for 1.5 #199

--- src/api/java/de/teamlapen/vampirism/api/entity/hunter/IHunterMob.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/hunter/IHunterMob.java
@@ -1,9 +1,10 @@
 package de.teamlapen.vampirism.api.entity.hunter;
 
 import de.teamlapen.vampirism.api.entity.IVampirismEntity;
+import net.minecraft.entity.monster.IMob;
 
 /**
  * Interface for all non player hunters
  */
-public interface IHunterMob extends IHunter, IVampirismEntity {
+public interface IHunterMob extends IHunter, IVampirismEntity, IMob {
 }
