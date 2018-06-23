5ef10e5c13f39c28842013fd9b153c3210b94ac9
max <maxanier@maxgb.de>
2018-06-23 16:44:17 +0200

    Make garlic plants give garlic effect to vampires

--- src/main/java/de/teamlapen/vampirism/blocks/BlockGarlic.java
+++ src/main/java/de/teamlapen/vampirism/blocks/BlockGarlic.java
@@ -1,13 +1,21 @@
 package de.teamlapen.vampirism.blocks;
 
+import de.teamlapen.vampirism.api.EnumStrength;
+import de.teamlapen.vampirism.api.VReference;
+import de.teamlapen.vampirism.api.entity.vampire.IVampire;
 import de.teamlapen.vampirism.core.ModItems;
+import de.teamlapen.vampirism.entity.DamageHandler;
+import de.teamlapen.vampirism.util.Helper;
 import de.teamlapen.vampirism.util.REFERENCE;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -34,6 +42,12 @@ public class BlockGarlic extends BlockCrops {
         return super.getDrops(world, pos, state, fortune - 1);
     }
 
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if (state.getValue(AGE) > 5 && Helper.isVampire(entityIn)) {
+            DamageHandler.affectVampireGarlicDirect(entityIn instanceof EntityPlayer ? VReference.VAMPIRE_FACTION.getPlayerCapability((EntityPlayer) entityIn) : (IVampire) entityIn, EnumStrength.WEAK);
+        }
+    }
 
     @Override
     protected Item getCrop() {
