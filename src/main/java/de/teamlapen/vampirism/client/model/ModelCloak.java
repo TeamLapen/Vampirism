1ca20a2d161cd48c1140750662bb54ed8e21783a
paulv <paulv@192.168.0.38>
2018-08-02 20:26:31 +0200

    new model for vampire cloak

--- src/main/java/de/teamlapen/vampirism/client/model/ModelCloak.java
+++ src/main/java/de/teamlapen/vampirism/client/model/ModelCloak.java
@@ -41,7 +41,7 @@ public class ModelCloak extends ModelBiped {
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		super.render(entity, f, f1, f2, f3, f4, f5);;
+		super.render(entity, f, f1, f2, f3, f4, f5);
 		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
 		setOffsets(f, f1, f2, f3, f4, f5, entity);
 		cloakback.render(f5);
