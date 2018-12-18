8ccc7cf746d9c306cf67a194a9f94c862abfe05f
cheaterpaul <thecheaterpaul@gmail.com>
2018-12-18 22:19:46 +0100

    generic type

--- src/main/java/de/teamlapen/vampirism/entity/action/actions/InvisibleEntityAction.java
+++ src/main/java/de/teamlapen/vampirism/entity/action/actions/InvisibleEntityAction.java
@@ -1,49 +1,36 @@
-package de.teamlapen.vampirism.entity.action.vampire;
+package de.teamlapen.vampirism.entity.action.actions;
 
-import de.teamlapen.vampirism.api.VReference;
+import de.teamlapen.vampirism.api.difficulty.IAdjustableLevel;
 import de.teamlapen.vampirism.api.entity.actions.DefaultEntityAction;
 import de.teamlapen.vampirism.api.entity.actions.ILastingAction;
-import de.teamlapen.vampirism.api.entity.factions.IFaction;
-import de.teamlapen.vampirism.api.entity.vampire.IVampireMob;
+import de.teamlapen.vampirism.api.entity.factions.IFactionEntity;
 import de.teamlapen.vampirism.config.Balance;
+import de.teamlapen.vampirism.entity.EntityVampirism;
 
-public class InvisibleVampireEntityAction extends DefaultEntityAction implements ILastingAction<IVampireMob> {
-    public InvisibleVampireEntityAction() {
-    }
-    
-    public boolean activate(IVampireMob entity) {
-        entity.getRepresentingEntity().setInvisible(true);
-        return true;
-    }
+public class InvisibleEntityAction<T extends EntityVampirism & IFactionEntity & IAdjustableLevel> extends DefaultEntityAction implements ILastingAction<T> {
     
     @Override
     public int getCooldown(int level) {
-        return Balance.ea.VAMPIRE_INVISIBLE_COOLDOWN * 20; // seconds into ticks
+        return Balance.ea.INVISIBLE_COOLDOWN * 20; // seconds into ticks
     }
 
     @Override
     public int getDuration(int level) {
-        return Balance.ea.VAMPIRE_INVISIBLE_DURATION;
+        return Balance.ea.INVISIBLE_DURATION;
     }
 
     @Override
-    public void deactivate(IVampireMob entity) {
+    public void deactivate(T entity) {
         entity.getRepresentingEntity().setInvisible(false);
         
     }
 
     @Override
-    public boolean onUpdate(IVampireMob entity) {
+    public boolean onUpdate(T entity) {
         if (!entity.getRepresentingEntity().isInvisible()) {
             entity.getRepresentingEntity().setInvisible(true);
         }
         return false;
     }
-
-    @Override
-    public IFaction getFaction() {
-        return VReference.VAMPIRE_FACTION;
-    }
-
 
 }
