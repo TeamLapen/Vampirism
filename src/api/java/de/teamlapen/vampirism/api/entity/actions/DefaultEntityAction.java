04fd00ef8d49d5428af8b5c18349e097f0932320
cheaterpaul <thecheaterpaul@gmail.com>
2018-12-18 22:25:27 +0100

    added for easy code in EntityAIUseAction class

--- src/api/java/de/teamlapen/vampirism/api/entity/actions/DefaultEntityAction.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/actions/DefaultEntityAction.java
@@ -4,4 +4,8 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public abstract class DefaultEntityAction extends IForgeRegistryEntry.Impl<IEntityAction> implements IEntityAction {
 
+    @Override
+    public int getDuration(int level) {
+        return getCooldown(level);
+    }
 }
