d78835242434bae37eb9c97fc49929de1a6063b7
cheaterpaul <thecheaterpaul@gmail.com>
2018-12-18 22:26:18 +0100

    rebuild

--- src/main/java/de/teamlapen/vampirism/entity/action/actions/HealEntityAction.java
+++ src/main/java/de/teamlapen/vampirism/entity/action/actions/HealEntityAction.java
@@ -1,30 +1,25 @@
-package de.teamlapen.vampirism.entity.action.vampire;
+package de.teamlapen.vampirism.entity.action.actions;
 
-import de.teamlapen.vampirism.api.VReference;
+import de.teamlapen.vampirism.api.difficulty.IAdjustableLevel;
 import de.teamlapen.vampirism.api.entity.actions.DefaultEntityAction;
 import de.teamlapen.vampirism.api.entity.actions.IInstantAction;
-import de.teamlapen.vampirism.api.entity.factions.IFaction;
-import de.teamlapen.vampirism.api.entity.vampire.IVampireMob;
+import de.teamlapen.vampirism.api.entity.factions.IFactionEntity;
 import de.teamlapen.vampirism.config.Balance;
-import net.minecraft.init.MobEffects;
-import net.minecraft.potion.PotionEffect;
+import de.teamlapen.vampirism.entity.EntityVampirism;
 
-public class HealVampireEntityAction extends DefaultEntityAction implements IInstantAction<IVampireMob> {
+public class HealEntityAction<T extends EntityVampirism & IFactionEntity & IAdjustableLevel> extends DefaultEntityAction implements IInstantAction<T> {
 
     @Override
     public int getCooldown(int level) {
-        return Balance.ea.VAMPIRE_HEAL_COOLDOWN * 20; // seconds into ticks
+        return Balance.ea.HEAL_COOLDOWN * 20; // seconds into ticks
     }
 
     @Override
-    public boolean activate(IVampireMob entity) {
-        entity.getRepresentingEntity().addPotionEffect(new PotionEffect(MobEffects.REGENERATION, Balance.ea.VAMPIRE_HEAL_AMOUNT, 0));
+    public boolean activate(T entity) {
+        System.out.println(entity.getRepresentingEntity().getHealth() + " - "); // TODO remove
+        entity.getRepresentingEntity().heal(entity.getMaxHealth() / Balance.ea.HEAL_AMOUNT);
+        System.out.print(entity.getRepresentingEntity().getHealth());// TODO remove
         return true;
-    }
-
-    @Override
-    public IFaction getFaction() {
-        return VReference.VAMPIRE_FACTION;
     }
 
 }
