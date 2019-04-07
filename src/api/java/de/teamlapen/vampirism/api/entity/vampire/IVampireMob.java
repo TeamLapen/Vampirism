1153206cbff65e17695010b61ed3c55387695c09
Maxanier <maxanier@maxgb.de>
2019-04-07 17:08:19 +0200

    Remove IMob again

--- src/api/java/de/teamlapen/vampirism/api/entity/vampire/IVampireMob.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/vampire/IVampireMob.java
@@ -1,12 +1,11 @@
 package de.teamlapen.vampirism.api.entity.vampire;
 
 import de.teamlapen.vampirism.api.entity.IVampirismEntity;
-import net.minecraft.entity.monster.IMob;
 
 /**
  * Interface for all non player vampires
  */
-public interface IVampireMob extends IVampire, IVampirismEntity, IMob {
+public interface IVampireMob extends IVampire, IVampirismEntity {
 
 
 }
