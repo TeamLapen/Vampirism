86ac7f84597884f02c9ae2bc4eaa427d9e30aca0
maxanier <dev@maxgb.de>
2019-11-24 23:48:04 +0100

    Close #509

--- src/main/java/de/teamlapen/vampirism/player/vampire/actions/BatVampireAction.java
+++ src/main/java/de/teamlapen/vampirism/player/vampire/actions/BatVampireAction.java
@@ -104,8 +104,10 @@ public class BatVampireAction extends DefaultVampireAction implements ILastingAc
 
     @Override
     public void onActivatedClient(IVampirePlayer vampire) {
-        setPlayerBat(vampire.getRepresentingPlayer(), true);
-        ((VampirePlayer) vampire).getSpecialAttributes().bat = true;
+        if (!((VampirePlayer) vampire).getSpecialAttributes().bat) {
+            setPlayerBat(vampire.getRepresentingPlayer(), true);
+            ((VampirePlayer) vampire).getSpecialAttributes().bat = true;
+        }
     }
 
     @Override
@@ -129,8 +131,10 @@ public class BatVampireAction extends DefaultVampireAction implements ILastingAc
     @Override
     public void onReActivated(IVampirePlayer vampire) {
         setModifier(vampire.getRepresentingPlayer(), true);
-        setPlayerBat(vampire.getRepresentingPlayer(), true);
-        ((VampirePlayer) vampire).getSpecialAttributes().bat = true;
+        if (!((VampirePlayer) vampire).getSpecialAttributes().bat) {
+            setPlayerBat(vampire.getRepresentingPlayer(), true);
+            ((VampirePlayer) vampire).getSpecialAttributes().bat = true;
+        }
     }
 
     @Override
