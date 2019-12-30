02a09d804add5d311033b96136914ca3ce6ae788
maxanier <dev@maxgb.de>
2019-12-30 11:48:03 +0100

    Add many new textures by TinkerHatWill. Allow infinite vampire textures Update README

--- src/main/java/de/teamlapen/vampirism/client/render/entities/BasicVampireRenderer.java
+++ src/main/java/de/teamlapen/vampirism/client/render/entities/BasicVampireRenderer.java
@@ -2,6 +2,7 @@ package de.teamlapen.vampirism.client.render.entities;
 
 import de.teamlapen.vampirism.entity.vampire.BasicVampireEntity;
 import de.teamlapen.vampirism.util.REFERENCE;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.BipedRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.model.BipedModel;
@@ -13,19 +14,15 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class BasicVampireRenderer extends BipedRenderer<BasicVampireEntity, BipedModel<BasicVampireEntity>> {
 
-    private static final ResourceLocation[] textures = {
-            new ResourceLocation(REFERENCE.MODID, "textures/entity/vampire.png"),
-            new ResourceLocation(REFERENCE.MODID, "textures/entity/vampire1.png"),
-            new ResourceLocation(REFERENCE.MODID, "textures/entity/vampire2.png"),
-            new ResourceLocation(REFERENCE.MODID, "textures/entity/vampire3.png"),
-    };
-
-    public static ResourceLocation getVampireTexture(int entityId) {
-        return textures[entityId % textures.length];
-    }
+    private final ResourceLocation[] textures;
 
     public BasicVampireRenderer(EntityRendererManager renderManagerIn) {
         super(renderManagerIn, new PlayerModel<>(0, false), 0.5F);
+        textures = Minecraft.getInstance().getResourceManager().getAllResourceLocations("textures/entity/vampire", s -> s.endsWith(".png")).stream().filter(r -> REFERENCE.MODID.equals(r.getNamespace())).toArray(ResourceLocation[]::new);
+    }
+
+    public ResourceLocation getVampireTexture(int entityId) {
+        return textures[entityId % textures.length];
     }
 
     @Override
