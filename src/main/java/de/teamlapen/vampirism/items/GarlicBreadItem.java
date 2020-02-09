d0a14476672163f200d19c38b6ab9ec26f159ab0
maxanier <dev@maxgb.de>
2020-02-09 13:30:14 +0100

    Allow garlic bread to cure sanguinare
    2cd89ec083b28bc5ca21c919be97058262898aa9

--- src/main/java/de/teamlapen/vampirism/items/GarlicBreadItem.java
+++ src/main/java/de/teamlapen/vampirism/items/GarlicBreadItem.java
@@ -1,12 +1,24 @@
 package de.teamlapen.vampirism.items;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class GarlicBreadItem extends VampirismItem {
     private static final String regName = "garlic_bread";
 
     public GarlicBreadItem() {
         super(regName, new Properties().food((new Food.Builder()).hunger(6).saturation(0.7F).build()).group(ItemGroup.FOOD));
+    }
+
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        if (!worldIn.isRemote) {
+            entityLiving.curePotionEffects(stack);
+        }
+        return super.onItemUseFinish(stack, worldIn, entityLiving);
     }
 }
