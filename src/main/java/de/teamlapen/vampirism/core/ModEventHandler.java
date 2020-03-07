8b5a942a64f4f74f25a1c49374f64d37e4414b55
maxanier <dev@maxgb.de>
2020-03-07 12:19:13 +0100

    Update alpha message. Close #620

--- src/main/java/de/teamlapen/vampirism/core/ModEventHandler.java
+++ src/main/java/de/teamlapen/vampirism/core/ModEventHandler.java
@@ -106,10 +106,10 @@ public class ModEventHandler {
             }
         }
         if (event.getPlayer().getRNG().nextInt(4) == 0) {
-            event.getPlayer().sendMessage(new StringTextComponent("You are running an alpha version of Vampirism for MC 1.14").applyTextStyle(TextFormatting.DARK_RED));
+            event.getPlayer().sendMessage(new StringTextComponent("You are running an alpha version of Vampirism for MC 1.15").applyTextStyle(TextFormatting.DARK_RED));
             event.getPlayer().sendMessage(new StringTextComponent("Some important parts are not working yet:"));
-            event.getPlayer().sendMessage(new StringTextComponent("- Guide book"));
             event.getPlayer().sendMessage(new StringTextComponent("- Rendering: There are still some rendering bugs. These should not affect gameplay though."));
+            event.getPlayer().sendMessage(new StringTextComponent("- Guide book"));
         }
 
 
