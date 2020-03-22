e59c0c1c85d9c1c87a3d779ee4714fb169aa64f4
Paul Becker <paube@mail.upb.de>
2020-03-23 00:10:04 +0100

    translation keys & task buider
    add task translationKeys & descriptionKeys & ITextComponents
    change from TaskRequirement.Builder to Task.Builder

--- src/api/java/de/teamlapen/vampirism/api/entity/player/task/TaskRequirement.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/player/task/TaskRequirement.java
@@ -1,19 +1,11 @@
 package de.teamlapen.vampirism.api.entity.player.task;
 
-import com.google.common.collect.ImmutableList;
-import net.minecraft.entity.EntityType;
-import net.minecraft.item.ItemStack;
-
 public class TaskRequirement {
 
     private final Type type;
 
     public TaskRequirement(Type type) {
         this.type = type;
-    }
-
-    public static Builder builder() {
-        return new Builder();
     }
 
     public Type getType() {
@@ -24,22 +16,4 @@ public class TaskRequirement {
         KILLS, ITEMS
     }
 
-    public static class Builder {
-
-        private final ImmutableList.Builder<TaskRequirement> requirements = ImmutableList.builder();
-
-        public Builder addEntityRequirement(EntityType<?> entityType, int amount) {
-            this.requirements.add(new KillRequirement(entityType, amount));
-            return this;
-        }
-
-        public Builder addItemRequirement(ItemStack itemStack) {
-            this.requirements.add(new ItemRequirement(itemStack));
-            return this;
-        }
-
-        public ImmutableList<TaskRequirement> build() {
-            return requirements.build();
-        }
-    }
 }
