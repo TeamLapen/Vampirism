9975886d6cda1082eda6b99573e2bf81d0a29763
Paul Becker <paube@mail.upb.de>
2020-03-22 23:31:46 +0100

    start TaskMasterScreen

--- src/main/java/de/teamlapen/vampirism/client/gui/TaskMasterScreen.java
+++ src/main/java/de/teamlapen/vampirism/client/gui/TaskMasterScreen.java
@@ -1,17 +1,62 @@
 package de.teamlapen.vampirism.client.gui;
 
+import de.teamlapen.vampirism.api.entity.player.task.Task;
 import de.teamlapen.vampirism.inventory.container.TaskMasterContainer;
+import de.teamlapen.vampirism.util.REFERENCE;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 public class TaskMasterScreen extends ContainerScreen<TaskMasterContainer> {
+    private static final ResourceLocation TASKMASTER_GUI_TEXTURE = new ResourceLocation(REFERENCE.MODID, "textures/gui/taskmaster.png");
+
+    private int scrolledTask;
+    private boolean mouseOnScroller;
+
     public TaskMasterScreen(TaskMasterContainer container, PlayerInventory playerInventory, ITextComponent containerName) {
         super(container, playerInventory, containerName);
     }
 
     @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_) {
+        super.drawGuiContainerForegroundLayer(p_146979_1_, p_146979_2_);
+    }
+
+    @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+    }
+
+    @Override
+    public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
+        super.render(p_render_1_, p_render_2_, p_render_3_);
+    }
+
+    private void renderToolTip(Task task, int x, int y) {
+
+    }
+
+    private void renderTask(Task task, int x, int y) {
+
+    }
+
+    private boolean isTaskListTooLong(int size) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseScrolled(double p_mouseScrolled_1_, double p_mouseScrolled_3_, double p_mouseScrolled_5_) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseDragged(double p_mouseDragged_1_, double p_mouseDragged_3_, int p_mouseDragged_5_, double p_mouseDragged_6_, double p_mouseDragged_8_) {
+        return super.mouseDragged(p_mouseDragged_1_, p_mouseDragged_3_, p_mouseDragged_5_, p_mouseDragged_6_, p_mouseDragged_8_);
     }
 
     @Override
