9ac56cdf406c52852c42cd4a4e1040d719a13751
maxanier <dev@maxgb.de>
2020-03-31 22:41:33 +0200

    WIP GuideAPI update

--- src/main/java/de/teamlapen/vampirism/modcompat/guide/GuideAPICompat.java
+++ src/main/java/de/teamlapen/vampirism/modcompat/guide/GuideAPICompat.java
@@ -1,9 +1,6 @@
 package de.teamlapen.vampirism.modcompat.guide;
 
 import de.teamlapen.lib.lib.util.IModCompat;
-import net.minecraftforge.common.config.ConfigCategory;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.event.FMLStateEvent;
 
 
 public class GuideAPICompat implements IModCompat {
@@ -12,14 +9,4 @@ public class GuideAPICompat implements IModCompat {
         return "guideapi";
     }
 
-    @Override
-    public void loadConfigs(Configuration config, ConfigCategory category) {
-
-    }
-
-
-    @Override
-    public void onInitStep(Step step, FMLStateEvent event) {
-
-    }
 }
