4de88d655f5169083e05b7886e5b7dbb8450c53e
maxanier <dev@maxgb.de>
2020-06-11 16:02:58 +0200

    Remove lord level on "leveldown"

--- src/main/java/de/teamlapen/vampirism/entity/factions/FactionPlayerHandler.java
+++ src/main/java/de/teamlapen/vampirism/entity/factions/FactionPlayerHandler.java
@@ -264,6 +264,8 @@ public class FactionPlayerHandler implements ISyncable.ISyncableEntityCapability
     public boolean setFactionAndLevel(IPlayableFaction<? extends IFactionPlayer<?>> faction, int level) {
         IPlayableFaction<? extends IFactionPlayer<?>> old = currentFaction;
         int oldLevel = currentLevel;
+        int newLordLevel = this.currentLordLevel;
+
         if (currentFaction != null && (!currentFaction.equals(faction) || level == 0)) {
             if (!currentFaction.getPlayerCapability(player).map(IFactionPlayer::canLeaveFaction).orElse(false)) {
                 LOGGER.info("You cannot leave faction {}, it is prevented by respective mod", currentFaction.getID());
@@ -281,17 +283,20 @@ public class FactionPlayerHandler implements ISyncable.ISyncableEntityCapability
         if (faction == null) {
             currentFaction = null;
             currentLevel = 0;
-            currentLordLevel = 0;
+            newLordLevel = 0;
         } else {
             currentFaction = faction;
             currentLevel = level;
             if (currentLevel != currentFaction.getHighestReachableLevel()) {
-                currentLordLevel = 0;
+                newLordLevel = 0;
             }
         }
         if (currentLevel == 0) {
             currentFaction = null;
-            currentLordLevel = 0;
+            newLordLevel = 0;
+        }
+        if (currentLordLevel != newLordLevel) {
+            this.setLordLevel(newLordLevel, false);
         }
         notifyFaction(old, oldLevel);
         sync(!Objects.equals(old, currentFaction));
@@ -304,6 +309,10 @@ public class FactionPlayerHandler implements ISyncable.ISyncableEntityCapability
 
     @Override
     public boolean setLordLevel(int level) {
+        return this.setLordLevel(level, true);
+    }
+
+    private boolean setLordLevel(int level, boolean sync) {
         if (currentFaction == null || currentLevel != currentFaction.getHighestReachableLevel()) {
             return false;
         }
@@ -317,7 +326,7 @@ public class FactionPlayerHandler implements ISyncable.ISyncableEntityCapability
                 c.setMaxMinions(this.getMaxMinions());
             }
         });
-        sync(false);
+        if (sync) sync(false);
         return true;
     }
 
