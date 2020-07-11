9a35d1a853d357dc6b521f2d9f1456648ecd89f0
Maxanier <dev@maxgb.de>
2020-07-11 18:25:12 +0200

    WIP replace crossbow infinity with vanilla infinity

--- src/main/java/de/teamlapen/vampirism/core/ModEnchantments.java
+++ src/main/java/de/teamlapen/vampirism/core/ModEnchantments.java
@@ -1,7 +1,6 @@
 package de.teamlapen.vampirism.core;
 
 import de.teamlapen.vampirism.items.enchantment.EnchantmentArrowFrugality;
-import de.teamlapen.vampirism.items.enchantment.EnchantmentCrossbowInfinite;
 import de.teamlapen.vampirism.items.enchantment.EnchantmentVampireSlayer;
 import de.teamlapen.vampirism.util.REFERENCE;
 import net.minecraft.enchantment.Enchantment;
@@ -14,14 +13,12 @@ import static de.teamlapen.lib.lib.util.UtilLib.getNull;
 @ObjectHolder(REFERENCE.MODID)
 public class ModEnchantments {
 
-    public static final EnchantmentCrossbowInfinite crossbowinfinite = getNull();
     public static final EnchantmentArrowFrugality crossbowfrugality = getNull();
     public static final EnchantmentVampireSlayer vampireslayer = getNull();
 
 
     static void registerEnchantments(IForgeRegistry<Enchantment> registry) {
         //Don't forget to add a enchantment description "enchantment.vampirism.<id>.desc" for new enchantments #624
-        registry.register(new EnchantmentCrossbowInfinite(Enchantment.Rarity.VERY_RARE));
         registry.register(new EnchantmentArrowFrugality(Enchantment.Rarity.VERY_RARE));
         registry.register(new EnchantmentVampireSlayer(Enchantment.Rarity.UNCOMMON));
     }
