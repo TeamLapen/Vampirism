d55bd5934ed166f381ee95b405b1757b3bc27856
maxanier <dev@maxgb.de>
2020-07-26 19:18:59 +0200

    Switch to blood_potion_table for now

--- src/main/java/de/teamlapen/vampirism/blocks/HunterTableBlock.java
+++ src/main/java/de/teamlapen/vampirism/blocks/HunterTableBlock.java
@@ -135,7 +135,7 @@ public class HunterTableBlock extends VampirismBlock {
         Block right = world.getBlockState(pos.offset(facing.rotateYCCW())).getBlock();
         Block front = world.getBlockState(pos.offset(facing.getOpposite())).getBlock();
         boolean weapon_table = left == ModBlocks.weapon_table || right == ModBlocks.weapon_table || behind == ModBlocks.weapon_table || front == ModBlocks.weapon_table;
-        boolean potion_table = left == ModBlocks.blood_container || right == ModBlocks.blood_container || behind == ModBlocks.blood_container || front == ModBlocks.blood_container;
+        boolean potion_table = left == ModBlocks.blood_potion_table || right == ModBlocks.blood_potion_table || behind == ModBlocks.blood_potion_table || front == ModBlocks.blood_potion_table; //TODO replace with extended potion table and update guide book
         boolean cauldron = left == ModBlocks.alchemical_cauldron || right == ModBlocks.alchemical_cauldron || behind == ModBlocks.alchemical_cauldron || front == ModBlocks.alchemical_cauldron;
 
         return getTierFor(weapon_table, potion_table, cauldron);
