c72216ec5fa4b0a1967ff462f2842766870aabc0
maxanier <dev@maxgb.de>
2020-08-02 14:15:23 +0200

    Add textures for hunter trainer and vampire task master by @PixelEyeStudios

--- src/main/java/de/teamlapen/vampirism/client/render/entities/HunterTrainerRenderer.java
+++ src/main/java/de/teamlapen/vampirism/client/render/entities/HunterTrainerRenderer.java
@@ -1,27 +1,25 @@
 package de.teamlapen.vampirism.client.render.entities;
 
-import com.google.common.base.Predicates;
-import de.teamlapen.vampirism.client.model.BasicHunterModel;
 import de.teamlapen.vampirism.client.model.HunterEquipmentModel;
-import de.teamlapen.vampirism.client.render.LayerCloak;
 import de.teamlapen.vampirism.client.render.LayerHunterEquipment;
 import de.teamlapen.vampirism.entity.hunter.HunterTrainerEntity;
 import de.teamlapen.vampirism.util.REFERENCE;
 import net.minecraft.client.renderer.entity.BipedRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class HunterTrainerRenderer extends BipedRenderer<HunterTrainerEntity, BasicHunterModel<HunterTrainerEntity>> {
-    private final ResourceLocation texture = new ResourceLocation(REFERENCE.MODID, "textures/entity/hunter_base1.png");
+public class HunterTrainerRenderer extends BipedRenderer<HunterTrainerEntity, PlayerModel<HunterTrainerEntity>> {
+    private final ResourceLocation texture = new ResourceLocation(REFERENCE.MODID, "textures/entity/hunter_trainer.png");
 
 
     public HunterTrainerRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new BasicHunterModel<>(), 0.5F);
+        super(renderManagerIn, new PlayerModel<>(0, false), 0.5F);
         this.addLayer(new LayerHunterEquipment<>(this, h -> HunterEquipmentModel.StakeType.ONLY, entityModel -> 1));
-        this.addLayer(new LayerCloak<>(this, texture, Predicates.alwaysTrue()));
+        //this.addLayer(new LayerCloak<>(this, texture, Predicates.alwaysTrue()));
 
     }
 
--- src/main/java/de/teamlapen/vampirism/client/render/entities/VampireTaskMasterRenderer.java
+++ src/main/java/de/teamlapen/vampirism/client/render/entities/VampireTaskMasterRenderer.java
@@ -1,40 +1,37 @@
 package de.teamlapen.vampirism.client.render.entities;
 
-import de.teamlapen.vampirism.client.core.ModEntitiesRender;
-import de.teamlapen.vampirism.entity.vampire.BasicVampireEntity;
 import de.teamlapen.vampirism.entity.vampire.VampireTaskMasterEntity;
 import de.teamlapen.vampirism.util.REFERENCE;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.BipedRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.model.BipedModel;
-import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.VillagerHeldItemLayer;
+import net.minecraft.client.renderer.entity.model.VillagerModel;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Render the advanced vampire with overlays
  */
 @OnlyIn(Dist.CLIENT)
-public class VampireTaskMasterRenderer extends BipedRenderer<VampireTaskMasterEntity, BipedModel<VampireTaskMasterEntity>> {
-    private final ResourceLocation[] textures;
+public class VampireTaskMasterRenderer extends MobRenderer<VampireTaskMasterEntity, VillagerModel<VampireTaskMasterEntity>> {
+
+    private final static ResourceLocation texture = new ResourceLocation(REFERENCE.MODID, "textures/entity/task_master_vampire.png");
 
     public VampireTaskMasterRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new PlayerModel<>(0F,false), 0.5F);
-        textures = Minecraft.getInstance().getResourceManager().getAllResourceLocations("textures/entity/vampire", s -> s.endsWith(".png")).stream().filter(r -> REFERENCE.MODID.equals(r.getNamespace())).toArray(ResourceLocation[]::new);
+        super(renderManagerIn, new VillagerModel<>(0f), 0.5F);
+        this.addLayer(new VillagerHeldItemLayer<>(this));
     }
 
-    public ResourceLocation getVampireTexture(int entityId) {
-        return textures[entityId % textures.length];
-    }
-
+    @Nullable
     @Override
     protected ResourceLocation getEntityTexture(VampireTaskMasterEntity entity) {
-        return getVampireTexture(entity.getEntityId());
+        return texture;
     }
+
 
     @Override
     protected void renderLivingLabel(@Nonnull VampireTaskMasterEntity entityIn, @Nonnull String str, double x, double y, double z, int maxDistance) {
