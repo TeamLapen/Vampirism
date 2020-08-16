61a146720ee698291178a97853b0f96788f94ec3
cheaterpaul <dev@paube.de>
2020-08-16 14:16:33 +0200

    fix minion names are out of sync

--- src/main/java/de/teamlapen/vampirism/entity/minion/VampireMinionEntity.java
+++ src/main/java/de/teamlapen/vampirism/entity/minion/VampireMinionEntity.java
@@ -167,6 +167,7 @@ public class VampireMinionEntity extends MinionEntity<VampireMinionEntity.Vampir
 
     @Override
     protected void onMinionDataReceived(@Nonnull VampireMinionData data) {
+        super.onMinionDataReceived(data);
     }
 
     @Override
