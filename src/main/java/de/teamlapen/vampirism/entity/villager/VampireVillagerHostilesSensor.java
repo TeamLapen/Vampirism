a6000883209a5330556726a9b9fb8cbbc24d21b2
cheaterpaul <dev@paube.de>
2020-08-31 16:24:57 +0200

    make zombie entities ignore vampire villager

---
+++ src/main/java/de/teamlapen/vampirism/entity/villager/VampireVillagerHostilesSensor.java
@@ -0,0 +1,33 @@
+package de.teamlapen.vampirism.entity.villager;
+
+import com.google.common.collect.Maps;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.sensor.VillagerHostilesSensor;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+
+public class VampireVillagerHostilesSensor extends VillagerHostilesSensor {
+    public static final Map<EntityType<?>, Float> hostiles;
+
+    static {
+        hostiles = Maps.newHashMap(VillagerHostilesSensor.field_220991_b);
+        hostiles.remove(EntityType.ZOMBIE);
+        hostiles.remove(EntityType.ZOMBIE_VILLAGER);
+        hostiles.remove(EntityType.DROWNED);
+        hostiles.remove(EntityType.HUSK);
+    }
+
+    @Override
+    protected boolean func_220987_a(@Nonnull LivingEntity villager, LivingEntity hostile) {
+        float f = hostiles.get(hostile.getType());
+        return hostile.getDistanceSq(villager) <= (double) (f * f);
+    }
+
+    @Override
+    protected boolean func_220988_c(LivingEntity hostile) {
+        return hostiles.containsKey(hostile.getType());
+    }
+
+}
