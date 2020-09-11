b3c54deabfdec02d261d3dc9e809ef69392cbd38
maxanier <dev@maxgb.de>
2020-09-11 12:04:11 +0200

    Close #768

--- src/main/java/de/teamlapen/vampirism/entity/villager/VampireVillagerHostilesSensor.java
+++ src/main/java/de/teamlapen/vampirism/entity/villager/VampireVillagerHostilesSensor.java
@@ -1,36 +1,41 @@
 package de.teamlapen.vampirism.entity.villager;
 
-import com.google.common.collect.Maps;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.sensor.VillagerHostilesSensor;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * just {@link VillagerHostilesSensor} with a hostile map without zombies
  */
 public class VampireVillagerHostilesSensor extends VillagerHostilesSensor {
-    public static final Map<EntityType<?>, Float> hostiles;
+    public static final Map<EntityType<?>, Float> HOSTILE_ENTITY_TYPES;
 
     static {
-        hostiles = Maps.newHashMap(VillagerHostilesSensor.field_220991_b);
-        hostiles.remove(EntityType.ZOMBIE);
-        hostiles.remove(EntityType.ZOMBIE_VILLAGER);
-        hostiles.remove(EntityType.DROWNED);
-        hostiles.remove(EntityType.HUSK);
+        //Adding entries will probably not work outside Dev as func_220988_c is not called for some reason
+        HOSTILE_ENTITY_TYPES = new HashMap<>(VillagerHostilesSensor.field_220991_b);
+        HOSTILE_ENTITY_TYPES.remove(EntityType.ZOMBIE);
+        HOSTILE_ENTITY_TYPES.remove(EntityType.ZOMBIE_VILLAGER);
+        HOSTILE_ENTITY_TYPES.remove(EntityType.DROWNED);
+        HOSTILE_ENTITY_TYPES.remove(EntityType.HUSK);
     }
 
     @Override
-    public boolean func_220987_a(@Nonnull LivingEntity villager, LivingEntity hostile) { //public to avoid AT issues
-        float f = hostiles.get(hostile.getType());
-        return hostile.getDistanceSq(villager) <= (double) (f * f);
+    public boolean func_220987_a(@Nonnull LivingEntity villagerEntity, LivingEntity hostileEntity) { //public to avoid AT issues
+        //func_220988_c is not checked first, so entries may not be present
+        @Nullable Float f = HOSTILE_ENTITY_TYPES.get(hostileEntity.getType()); //Careful about unboxing nullpointer
+        if (f == null) return false;
+        return hostileEntity.getDistanceSq(villagerEntity) <= (double) (f * f);
     }
 
+
     @Override
-    public boolean func_220988_c(LivingEntity hostile) { //public to avoid AT issues
-        return hostiles.containsKey(hostile.getType());
+    public boolean func_220988_c(LivingEntity hostile) { //For some reason this method is not called (as it does not properly override somehow maybe) outside dev
+        return HOSTILE_ENTITY_TYPES.containsKey(hostile.getType());
     }
 
 }
