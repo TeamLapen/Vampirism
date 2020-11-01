9245c55938a48438b13dfb2bea868551a1ff2491
maxanier <dev@maxgb.de>
2020-11-01 18:38:03 +0100

    Add GuideBook hook

---
+++ src/main/java/de/teamlapen/vampirism/modcompat/guide/VampirismGuideBookCategoriesEvent.java
@@ -0,0 +1,15 @@
+package de.teamlapen.vampirism.modcompat.guide;
+
+import de.maxanier.guideapi.api.impl.abstraction.CategoryAbstract;
+import net.minecraftforge.eventbus.api.Event;
+
+import java.util.List;
+
+public class VampirismGuideBookCategoriesEvent extends Event {
+
+    public final List<CategoryAbstract> categories;
+
+    public VampirismGuideBookCategoriesEvent(List<CategoryAbstract> categories) {
+        this.categories = categories;
+    }
+}
