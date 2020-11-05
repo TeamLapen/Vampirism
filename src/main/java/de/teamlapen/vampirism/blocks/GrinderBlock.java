7b9a077008a3cb4097025d7b435f6ba0a95a6a5d
cheaterpaul <dev@paube.de>
2020-11-05 17:52:01 +0100

    add clearContainer method to VampirismBlockContainer
    fix #800

--- src/main/java/de/teamlapen/vampirism/blocks/GrinderBlock.java
+++ src/main/java/de/teamlapen/vampirism/blocks/GrinderBlock.java
@@ -119,6 +119,11 @@ public class GrinderBlock extends VampirismBlockContainer {
     }
 
     @Override
+    protected void clearContainer(BlockState state, World worldIn, BlockPos pos) {
+        dropInventoryTileEntityItems(worldIn, pos);
+    }
+
+    @Override
     public BlockState rotate(BlockState state, Rotation rot) {
         return state.with(FACING, rot.rotate(state.get(FACING)));
     }
