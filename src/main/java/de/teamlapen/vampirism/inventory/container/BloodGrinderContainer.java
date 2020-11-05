086e2bad90c2e269c2c5c18d6b22b2fea8b046c7
cheaterpaul <dev@paube.de>
2020-11-05 18:17:41 +0100

    modify InventoryContainer itemstack move behavior
    - make better use of Container#transferStackInSlot for non player inventory slots

--- src/main/java/de/teamlapen/vampirism/inventory/container/BloodGrinderContainer.java
+++ src/main/java/de/teamlapen/vampirism/inventory/container/BloodGrinderContainer.java
@@ -3,11 +3,9 @@ package de.teamlapen.vampirism.inventory.container;
 import de.teamlapen.lib.lib.inventory.InventoryContainer;
 import de.teamlapen.vampirism.api.general.BloodConversionRegistry;
 import de.teamlapen.vampirism.core.ModContainer;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IWorldPosCallable;
 
@@ -27,42 +25,42 @@ public class BloodGrinderContainer extends InventoryContainer {
         this.addPlayerSlots(playerInventory);
     }
 
-    @Override
-    public ItemStack transferStackInSlot(PlayerEntity playerEntity, int index) {
-        ItemStack result = ItemStack.EMPTY;
-        Slot slot = this.inventorySlots.get(index);
-        if (slot != null && slot.getHasStack()) {
-            ItemStack slotStack = slot.getStack();
-            result = slotStack.copy();
-            if (index >= 1) {
-                if (index < 27) {
-                    if (!this.mergeItemStack(slotStack, 0, 1, false)) {
-                        return ItemStack.EMPTY;
-                    } else if (this.mergeItemStack(slotStack, 27, 36, false)) {
-                        return ItemStack.EMPTY;
-                    }
-                } else {
-                    if (!this.mergeItemStack(slotStack, 0, 27, false)) {
-                        return ItemStack.EMPTY;
-                    }
-                }
-            } else if (!this.mergeItemStack(slotStack, 1, 36, false)) {
-                return ItemStack.EMPTY;
-            }
-
-            if (slotStack.isEmpty()) {
-                slot.putStack(ItemStack.EMPTY);
-            } else {
-                slot.onSlotChanged();
-            }
-
-            if (slotStack.getCount() == result.getCount()) {
-                return ItemStack.EMPTY;
-            }
-
-            slot.onTake(playerEntity, slotStack);
-        }
-
-        return result;
-    }
+//    @Override
+//    public ItemStack transferStackInSlot(PlayerEntity playerEntity, int index) {
+//        ItemStack result = ItemStack.EMPTY;
+//        Slot slot = this.inventorySlots.get(index);
+//        if (slot != null && slot.getHasStack()) {
+//            ItemStack slotStack = slot.getStack();
+//            result = slotStack.copy();
+//            if (index >= 1) {
+//                if (index < 27) {
+//                    if (!this.mergeItemStack(slotStack, 0, 1, false)) {
+//                        return ItemStack.EMPTY;
+//                    } else if (this.mergeItemStack(slotStack, 27, 36, false)) {
+//                        return ItemStack.EMPTY;
+//                    }
+//                } else {
+//                    if (!this.mergeItemStack(slotStack, 0, 27, false)) {
+//                        return ItemStack.EMPTY;
+//                    }
+//                }
+//            } else if (!this.mergeItemStack(slotStack, 1, 36, false)) {
+//                return ItemStack.EMPTY;
+//            }
+//
+//            if (slotStack.isEmpty()) {
+//                slot.putStack(ItemStack.EMPTY);
+//            } else {
+//                slot.onSlotChanged();
+//            }
+//
+//            if (slotStack.getCount() == result.getCount()) {
+//                return ItemStack.EMPTY;
+//            }
+//
+//            slot.onTake(playerEntity, slotStack);
+//        }
+//
+//        return result;
+//    }
 }
