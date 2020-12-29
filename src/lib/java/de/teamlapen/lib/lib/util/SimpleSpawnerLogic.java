10ae24225f14af92d9567d5c365b69ab8cda207c
maxanier <dev@maxgb.de>
2020-12-29 12:41:34 +0100

    Fix #822

--- src/lib/java/de/teamlapen/lib/lib/util/SimpleSpawnerLogic.java
+++ src/lib/java/de/teamlapen/lib/lib/util/SimpleSpawnerLogic.java
@@ -180,8 +180,9 @@ public class SimpleSpawnerLogic<T extends Entity> {
                     }
 
                     if (limitType != null) {
+                        @Nullable
                         WorldEntitySpawner.EntityDensityManager densityManager = ((ServerWorld) this.world).getChunkProvider().func_241101_k_();
-                        if (!densityManager.func_234991_a_(limitType)) {
+                        if (densityManager != null && !densityManager.func_234991_a_(limitType)) {
                             this.resetTimer();
                             break;
                         }
