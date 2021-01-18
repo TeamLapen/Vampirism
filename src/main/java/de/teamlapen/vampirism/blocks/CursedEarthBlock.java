81e55f6ca86ad1d4ea7da0b908f869b762b0a5e4
maxanier <dev@maxgb.de>
2021-01-18 22:24:14 +0100

    Allow converting cursed earth to regular grass using holy water bottles (non-splash)

--- src/main/java/de/teamlapen/vampirism/blocks/CursedEarthBlock.java
+++ src/main/java/de/teamlapen/vampirism/blocks/CursedEarthBlock.java
@@ -2,10 +2,18 @@ package de.teamlapen.vampirism.blocks;
 
 import de.teamlapen.vampirism.api.VReference;
 import de.teamlapen.vampirism.core.ModBlocks;
+import de.teamlapen.vampirism.core.ModItems;
+import de.teamlapen.vampirism.items.HolyWaterBottleItem;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
@@ -95,6 +103,21 @@ public class CursedEarthBlock extends VampirismBlock implements IGrowable {
 
     @Override
     public void onPlantGrow(BlockState state, IWorld world, BlockPos pos, BlockPos source) {
-            world.setBlockState(pos, ModBlocks.cursed_earth.getDefaultState(), 2);
+        world.setBlockState(pos, ModBlocks.cursed_earth.getDefaultState(), 2);
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        ItemStack heldItemStack = player.getHeldItem(handIn);
+        Item heldItem = heldItemStack.getItem();
+        if (heldItem instanceof HolyWaterBottleItem) {
+            int uses = heldItem == ModItems.holy_water_bottle_ultimate ? 100 : (heldItem == ModItems.holy_water_bottle_enhanced ? 50 : 25);
+            if (player.getRNG().nextInt(uses) == 0) {
+                heldItemStack.setCount(heldItemStack.getCount() - 1);
+            }
+            worldIn.setBlockState(pos, Blocks.GRASS_BLOCK.getDefaultState());
+            return ActionResultType.SUCCESS;
+        }
+        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
     }
 }
