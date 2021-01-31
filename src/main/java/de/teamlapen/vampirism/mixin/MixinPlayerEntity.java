e3c963ef6d086bc6ebeee57135bc1f85a13bd442
maxanier <dev@maxgb.de>
2021-01-31 13:33:25 +0100

    Remove log line

--- src/main/java/de/teamlapen/vampirism/mixin/MixinPlayerEntity.java
+++ src/main/java/de/teamlapen/vampirism/mixin/MixinPlayerEntity.java
@@ -3,7 +3,6 @@ package de.teamlapen.vampirism.mixin;
 import de.teamlapen.vampirism.core.ModAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.player.PlayerEntity;
-import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinPlayerEntity {
     @Inject(method = "func_234570_el_", at = @At("RETURN"))
     private static void registerCustomAttributes(CallbackInfoReturnable<AttributeModifierMap.MutableAttribute> cir) {
-        LogManager.getLogger().info("Adding additional {}", ModAttributes.sundamage);
         cir.getReturnValue().createMutableAttribute(ModAttributes.sundamage).createMutableAttribute(ModAttributes.blood_exhaustion).createMutableAttribute(ModAttributes.bite_damage);
     }
 }
