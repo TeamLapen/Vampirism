4906fb8bd6fecbb4fca36f8085ab439c60c3f696
Paul Becker <dev@paube.de>
2021-02-06 16:10:24 +0100

    Use bell to highlight capture entities (#846)

---
+++ src/main/java/de/teamlapen/vampirism/mixin/MixinBellBlock.java
@@ -0,0 +1,21 @@
+package de.teamlapen.vampirism.mixin;
+
+import de.teamlapen.vampirism.tileentity.TotemHelper;
+import net.minecraft.block.BellBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BellBlock.class)
+public class MixinBellBlock {
+
+    @Inject(method = "attemptRing", at = @At(value = "RETURN", ordinal = 0))
+    public void ringTotem(World world, BlockState state, BlockRayTraceResult result, PlayerEntity player, boolean canRingBell, CallbackInfoReturnable<Boolean> cir){
+        TotemHelper.ringBell(world, player);
+    }
+}
