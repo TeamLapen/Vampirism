4fac9aa72da00015697761d252772ef3cf721cab
maxanier <dev@maxgb.de>
2021-02-13 13:40:54 +0100

    Fix error while saving

--- src/main/java/de/teamlapen/vampirism/mixin/MixinLivingEntity.java
+++ src/main/java/de/teamlapen/vampirism/mixin/MixinLivingEntity.java
@@ -8,8 +8,6 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,8 +17,6 @@ import javax.annotation.Nullable;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
-    private final static Logger LOGGER = LogManager.getLogger();
-
     @Deprecated
     protected MixinLivingEntity(EntityType<? extends LivingEntity> type, World worldIn) {
         super(type, worldIn);
