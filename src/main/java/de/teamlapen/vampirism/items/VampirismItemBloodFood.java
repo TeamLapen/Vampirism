0aa532291ba52bfee13d65715e05a41e9d639c82
maxanier <dev@maxgb.de>
2021-02-27 13:18:44 +0100

    Reduce hunger replenishment of blood food. Add nausea effect for non vampires
    (cherry picked from commit 67533e4ab5e2d16935bad6c66db09eb40dcd408b)
    
    (cherry picked from commit 67015cd83af82a2aac6ba2d08857ea2b81a4a511)

--- src/main/java/de/teamlapen/vampirism/items/VampirismItemBloodFood.java
+++ src/main/java/de/teamlapen/vampirism/items/VampirismItemBloodFood.java
@@ -2,10 +2,13 @@ package de.teamlapen.vampirism.items;
 
 import de.teamlapen.vampirism.VampirismMod;
 import de.teamlapen.vampirism.player.vampire.VampirePlayer;
+import de.teamlapen.vampirism.util.Helper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
@@ -13,8 +16,11 @@ import net.minecraft.world.World;
 
 public class VampirismItemBloodFood extends VampirismItem {
 
-    public VampirismItemBloodFood(String regName, Food food) {
-        super(regName, new Properties().group(VampirismMod.creativeTab).food(food));
+    private final Food vampireFood;
+
+    public VampirismItemBloodFood(String regName, Food vampireFood, Food humanFood) {
+        super(regName, new Properties().group(VampirismMod.creativeTab).food(humanFood));
+        this.vampireFood = vampireFood;
     }
 
     @Override
@@ -22,9 +28,12 @@ public class VampirismItemBloodFood extends VampirismItem {
         if (entityLiving instanceof PlayerEntity) {
             assert stack.getItem().getFood() != null;//Don't shrink stack before retrieving food
             PlayerEntity player = (PlayerEntity) entityLiving;
-            VampirePlayer.getOpt(player).ifPresent(v -> v.drinkBlood(stack.getItem().getFood().getHealing(), stack.getItem().getFood().getSaturation()));
+            VampirePlayer.getOpt(player).ifPresent(v -> v.drinkBlood(vampireFood.getHealing(), vampireFood.getSaturation()));
             worldIn.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
-            entityLiving.onFoodEaten(worldIn, stack); //Shrinks stack
+            entityLiving.onFoodEaten(worldIn, stack); //Shrinks stack and applies human food effects
+            if (!Helper.isVampire(player)) {
+                player.addPotionEffect(new EffectInstance(Effects.NAUSEA, 20 * 20));
+            }
         }
         return stack;
     }
