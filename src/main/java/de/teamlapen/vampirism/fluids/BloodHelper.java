f6e96f141fb2c078f03305a989d3e12cb268bdc6
maxanier <dev@maxgb.de>
2021-04-02 17:37:38 +0200

    Maybe help with #905
    (cherry picked from commit 38f66831ce481b9905719982f0d4ec84730bf611)

--- src/main/java/de/teamlapen/vampirism/fluids/BloodHelper.java
+++ src/main/java/de/teamlapen/vampirism/fluids/BloodHelper.java
@@ -27,6 +27,7 @@ public class BloodHelper {
     private static final Logger LOGGER = LogManager.getLogger(BloodHelper.class);
 
     /**
+     * TODO 1.17 delete
      * Returns the first stack on the players hotbar that can store blood
      *
      * @param inventory
@@ -36,7 +37,7 @@ public class BloodHelper {
         int hotbarSize = PlayerInventory.getHotbarSize();
         for (int i = 0; i < hotbarSize; i++) {
             ItemStack stack = inventory.getStackInSlot(i);
-            if (!stack.isEmpty() && canStoreBlood(stack)) return stack;
+            if (!stack.isEmpty() && FluidUtil.getFluidHandler(stack).map(handler -> handler.fill(new FluidStack(ModFluids.blood, 1000), IFluidHandler.FluidAction.SIMULATE) > 0).orElse(false)) return stack;
         }
         return ItemStack.EMPTY;
     }
@@ -45,14 +46,11 @@ public class BloodHelper {
      * Checks if the given stack can store blood
      */
     public static boolean canStoreBlood(@Nonnull ItemStack stack) {
-        return FluidUtil.getFluidHandler(stack).map(handler -> handler.fill(new FluidStack(ModFluids.blood, 1000), IFluidHandler.FluidAction.SIMULATE) > 0).orElse(false);
+        return fill(stack, 1000, IFluidHandler.FluidAction.SIMULATE)>0;
     }
 
     /**
-     * Returns the first glas bottle stack on the players hotbar
-     *
-     * @param inventory
-     * @return
+     * Returns the first glass bottle stack on the players hotbar
      */
     public static ItemStack getGlassBottleInHotbar(PlayerInventory inventory) {
         int hotbarSize = PlayerInventory.getHotbarSize();
@@ -111,34 +109,41 @@ public class BloodHelper {
     /**
      * Fills the blood in container in the players inventory using multiple possible ways
      *
-     * @param player
      * @param amt    Fluid amount in mB
      * @return Blood amount that could not be filled
      */
     public static int fillBloodIntoInventory(PlayerEntity player, int amt) {
         if (amt <= 0) return 0;
-        ItemStack stack = getBloodHandlerInHotbar(player.inventory);
+        ItemStack stack = ItemStack.EMPTY;
+        int hotbarSize = PlayerInventory.getHotbarSize();
+        for (int i = 0; i < hotbarSize; i++) {
+            ItemStack stack1 = player.inventory.getStackInSlot(i);
+            if (!stack1.isEmpty() && fill(stack1, amt, IFluidHandler.FluidAction.SIMULATE)>0) {
+                stack = stack1;
+                break;
+            }
+        }
         if (!stack.isEmpty()) {
             int filled = fill(stack, amt, IFluidHandler.FluidAction.EXECUTE);
             if (filled > 0) {
                 if (filled < amt) {
                     return fillBloodIntoInventory(player, amt - filled);
-
                 } else {
                     return 0;
                 }
             }
+            LOGGER.warn("Could not execute bottle fill even though simulation was successful. Item: {} Amount: {}",stack,amt);
         }
-        ItemStack glas = getGlassBottleInHotbar(player.inventory);
-        if (!glas.isEmpty() && VampirismConfig.COMMON.autoConvertGlassBottles.get()) {
+        ItemStack glass = getGlassBottleInHotbar(player.inventory);
+        if (!glass.isEmpty() && VampirismConfig.COMMON.autoConvertGlassBottles.get()) {
             ItemStack bloodBottle = new ItemStack(ModItems.blood_bottle, 1);
             int filled = fill(bloodBottle, amt, IFluidHandler.FluidAction.EXECUTE);
             if (filled == 0) {
                 LOGGER.warn("Failed to fill blood bottle with blood");
             }
-            glas.shrink(1);
-            if (glas.isEmpty()) {
-                player.inventory.deleteStack(glas);
+            glass.shrink(1);
+            if (glass.isEmpty()) {
+                player.inventory.deleteStack(glass);
             }
             if (!player.inventory.addItemStackToInventory(bloodBottle)) {
                 player.dropItem(bloodBottle, false);
