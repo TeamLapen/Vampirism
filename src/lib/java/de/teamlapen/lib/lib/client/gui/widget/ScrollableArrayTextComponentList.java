4db12f6af3986367d17929b6f94db31ddc02083a
maxanier <dev@maxgb.de>
2021-08-14 18:52:06 +0200

    Fix ScrollableArrayTextComponentList always using the id of the first of multiple identically named items. E.g. multiple identically named minions

--- src/lib/java/de/teamlapen/lib/lib/client/gui/widget/ScrollableArrayTextComponentList.java
+++ src/lib/java/de/teamlapen/lib/lib/client/gui/widget/ScrollableArrayTextComponentList.java
@@ -9,19 +9,21 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class ScrollableArrayTextComponentList extends ScrollableListWidget<Pair<Integer, ITextComponent>> {
 
     private static Collection<Pair<Integer, ITextComponent>> getItems(Supplier<ITextComponent[]> baseValueSupplier) {
-        List<ITextComponent> list = Arrays.asList(baseValueSupplier.get());
-        return list.stream().map(item -> Pair.of(list.indexOf(item), item)).collect(Collectors.toList());
+        Collection<Pair<Integer, ITextComponent>> items = new ArrayList<>();
+        ITextComponent[] baseValues = baseValueSupplier.get();
+        for (int i = 0; i < baseValues.length; i++) {
+            items.add(Pair.of(i, baseValues[i]));
+        }
+        return items;
     }
 
     private static Supplier<ITextComponent[]> createTextArray(int amount, TextComponent baseName) {
