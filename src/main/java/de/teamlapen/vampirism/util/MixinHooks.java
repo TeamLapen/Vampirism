c507a432af7fd2835cf892995c5780f2303abf2f
cheaterpaul <dev@paube.de>
2021-10-09 14:12:50 +0200

    add village building limits to RepurposedStructures
    (cherry picked from commit 4d60d875dd44c0e2a43aadf1e12e8d090db321d3)

--- src/main/java/de/teamlapen/vampirism/util/MixinHooks.java
+++ src/main/java/de/teamlapen/vampirism/util/MixinHooks.java
@@ -1,10 +1,16 @@
 package de.teamlapen.vampirism.util;
 
 import com.google.common.collect.Lists;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
 import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
+import net.minecraftforge.fml.ModList;
+import org.apache.logging.log4j.LogManager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class MixinHooks {
@@ -19,13 +25,35 @@ public class MixinHooks {
      */
     public static boolean enforcingGlowing_bloodVision = false;
 
-    public static void addSingleInstanceStructure(List<String> structures) {
-        onlyOneStructure.addAll(structures);
+    public static void addSingleInstanceStructure(List<ResourceLocation> structures) {
+        onlyOneStructure.addAll(structures.stream().map(MixinHooks::singleJigsawString).collect(Collectors.toList()));
+        addSingleInstanceStructureToRepurposedStructures(structures);
+    }
+
+    /**
+     * adds structures to the limit list of Repurposed Structures
+     */
+    private static void addSingleInstanceStructureToRepurposedStructures(List<ResourceLocation> structures) {
+        try {
+            if (ModList.get().isLoaded("repurposed_structures")) {
+                // get piece count list from rs
+                // https://github.com/TelepathicGrunt/RepurposedStructures/blob/e1bfd9e79bade3d020fcdd443b387f96d209a519/src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/pieces/StructurePiecesBehavior.java#L90
+                // noinspection unchecked
+                Map<ResourceLocation, Integer> repurposed_structures_limit = (Map<ResourceLocation, Integer>) Class.forName("com.telepathicgrunt.repurposedstructures.world.structures.pieces.StructurePiecesBehavior").getField("PIECES_COUNT").get(null);
+                repurposed_structures_limit.putAll(structures.stream().collect(HashMap::new, (map, loc) -> map.put(loc, 1), HashMap::putAll));
+            }
+        } catch (Exception error) {
+            LogManager.getLogger().error("Could not add village building to RepurposedStructures limit list", error);
+        }
     }
 
     public static boolean checkStructures(List<? super PoolElementStructurePiece> pieces, StructurePoolElement jigsawPiece) {
         if (!onlyOneStructure.contains(jigsawPiece.toString())) return false;
         return pieces.stream().anyMatch(structurePiece -> onlyOneStructure.stream().anyMatch(string -> ((PoolElementStructurePiece) structurePiece).getElement().toString().equals(string)));
+    }
+
+    private static String singleJigsawString(ResourceLocation resourceLocation) {
+        return "Single[Left[" + resourceLocation.toString() + "]]";
     }
 
 }
