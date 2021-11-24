271472093a1f6d52f1cf843ae5f35245e3c1a161
cheaterpaul <dev@paube.de>
2021-11-24 22:55:31 +0100

    create faction builder

---
+++ src/api/java/de/teamlapen/vampirism/api/entity/factions/IFactionBuilder.java
@@ -0,0 +1,37 @@
+package de.teamlapen.vampirism.api.entity.factions;
+
+import javax.annotation.Nullable;
+import java.awt.*;
+
+public interface IFactionBuilder<T extends IFactionEntity> {
+
+    /**
+     * Sets the faction color
+     *
+     * @param color Color e.g. for level rendering
+     * @return the builder
+     */
+    IFactionBuilder<T> color(Color color);
+
+    /**
+     * Sets this faction as hostile to neutral entities
+     *
+     * @return the builder
+     */
+    IFactionBuilder<T> hostileTowardsNeutral();
+
+    /**
+     * Adds faction village compatibility
+     *
+     * @param villageFactionData village capture related utility class (if null will gets filled with dummy)
+     * @return the builder
+     */
+    IFactionBuilder<T> village(@Nullable IVillageFactionData villageFactionData);
+
+    /**
+     * finish the building and registers the faction with values from the builder
+     *
+     * @return the final faction
+     */
+    IFaction<T> register();
+}
