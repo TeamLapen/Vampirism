c867844a89e06f7140afeb000c7850c8d97a11b2
maxanier <dev@maxgb.de>
2022-01-02 11:30:25 +0100

    Fix coffin model

--- src/main/java/de/teamlapen/vampirism/client/model/CoffinModel.java
+++ src/main/java/de/teamlapen/vampirism/client/model/CoffinModel.java
@@ -47,8 +47,8 @@ public class CoffinModel extends Model {
     public static LayerDefinition createLayer() {
         MeshDefinition mesh = new MeshDefinition();
         PartDefinition part = mesh.getRoot();
-        part.addOrReplaceChild(LEFT_PLATE, CubeListBuilder.create().texOffs(0, 64).mirror().addBox(-8, -12, 0, 1, 12, 32), PartPose.offset(0, 23, -8));
-        part.addOrReplaceChild(RIGHT_PLATE, CubeListBuilder.create().texOffs(66, 64).mirror().addBox(7, -12, 0, 1, 12, 32), PartPose.offset(0, 23, -8));
+        part.addOrReplaceChild(LEFT_PLATE, CubeListBuilder.create().texOffs(0, 64).mirror().addBox(7, -12, 0, 1, 12, 32), PartPose.offset(0, 23, -8));
+        part.addOrReplaceChild(RIGHT_PLATE, CubeListBuilder.create().texOffs(66, 64).mirror().addBox(-8, -12, 0, 1, 12, 32), PartPose.offset(0, 23, -8));
         part.addOrReplaceChild(BACK_PLATE, CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-8, 0, 0, 16, 1, 32), PartPose.offset(0, 23, -8));
         part.addOrReplaceChild(TOP_PLATE, CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-7, -12, 31, 14, 12, 1), PartPose.offset(0, 23, -8));
         part.addOrReplaceChild(BOTTOM_PLATE, CubeListBuilder.create().texOffs(0, 15).mirror().addBox(-7, -11, 0, 14, 12, 1), PartPose.offset(0, 22, -8));
