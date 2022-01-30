6a36e51d0072f24b3412a09de909a6d56f14eb2f
maxanier <dev@maxgb.de>
2022-01-30 14:07:02 +0100

    Fix NPE when bound action is removed

--- src/main/java/de/teamlapen/vampirism/entity/factions/FactionPlayerHandler.java
+++ src/main/java/de/teamlapen/vampirism/entity/factions/FactionPlayerHandler.java
@@ -456,20 +456,30 @@ public class FactionPlayerHandler implements ISyncable.ISyncableEntityCapability
     }
 
     private void loadBoundActions(CompoundNBT nbt) {
+        // Read bound actions from legacy format
         if (nbt.contains("bound1")) {
-            this.boundActions.put(1, ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound1"))));
+            IAction i = ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound1")));
+            if(i!=null)this.boundActions.put(1,i);
         }
         if (nbt.contains("bound2")) {
-            this.boundActions.put(2, ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound2"))));
+            IAction i = ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound2")));
+            if(i!=null)this.boundActions.put(2,i);
         }
         if (nbt.contains("bound3")) {
-            this.boundActions.put(3, ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound3"))));
+            IAction i = ModRegistries.ACTIONS.getValue(new ResourceLocation(nbt.getString("bound3")));
+            if(i!=null)this.boundActions.put(3,i);
         }
+        // Read bound actions from new format
         CompoundNBT bounds = nbt.getCompound("bound_actions");
         for (String s : bounds.getAllKeys()) {
             int id = Integer.parseInt(s);
             IAction action = ModRegistries.ACTIONS.getValue(new ResourceLocation(bounds.getString(s)));
-            this.boundActions.put(id, action);
+            if(action == null){
+                LOGGER.warn("Cannot find bound action {}", bounds.getString(s));
+            }
+            else{
+                this.boundActions.put(id, action);
+            }
         }
     }
 
