1ee6d55a6e475f9f278c03946a41b4b5c7c3697c
cheaterpaul <dev@paube.de>
2022-06-21 23:37:49 +0200

    fix hissing sound player only for hissing player

--- src/main/java/de/teamlapen/vampirism/player/vampire/actions/HissingAction.java
+++ src/main/java/de/teamlapen/vampirism/player/vampire/actions/HissingAction.java
@@ -8,6 +8,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
 import net.minecraft.world.entity.ai.goal.target.TargetGoal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
 
 public class HissingAction extends DefaultVampireAction {
@@ -24,7 +25,8 @@ public class HissingAction extends DefaultVampireAction {
 
     @Override
     protected boolean activate(IVampirePlayer vampire) {
-        vampire.getRepresentingPlayer().playNotifySound(ModSounds.ENTITY_VAMPIRE_SCREAM.get(), SoundSource.PLAYERS, 1, 1);
+        Player entity = vampire.getRepresentingPlayer();
+        entity.level.playSound(null, entity.getX(), entity.getY(), entity.getZ(), ModSounds.ENTITY_VAMPIRE_SCREAM.get(), SoundSource.PLAYERS,1,1);
         vampire.getRepresentingPlayer().getCommandSenderWorld().getEntitiesOfClass(Mob.class, new AABB(vampire.getRepresentingPlayer().blockPosition()).inflate(10, 10, 10)).forEach(e -> {
             if (e.getTarget() == vampire.getRepresentingPlayer()) {
                 e.targetSelector.getRunningGoals().filter(g -> g.getGoal() instanceof TargetGoal).forEach(WrappedGoal::stop);
