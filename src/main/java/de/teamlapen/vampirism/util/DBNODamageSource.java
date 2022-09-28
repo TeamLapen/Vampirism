07fc39b5f0e65cd7f4d38c1d8863ef56d9e137ec
maxanier <dev@maxgb.de>
2022-09-28 17:37:56 +0200

    Change damage source when giving up in DBNO to a) display an appropriate message and b) better compatibility with PlayerRevive

---
+++ src/main/java/de/teamlapen/vampirism/util/DBNODamageSource.java
@@ -0,0 +1,26 @@
+package de.teamlapen.vampirism.util;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+
+import javax.annotation.Nonnull;
+
+
+public class DBNODamageSource extends DamageSource {
+
+    @Nonnull
+    private final Component originalSource;
+    public DBNODamageSource(@Nonnull Component originalSource) {
+        super("vampirism_dbno");
+        this.originalSource = originalSource;
+    }
+
+    @Nonnull
+    @Override
+    public Component getLocalizedDeathMessage(@Nonnull LivingEntity entityLivingBaseIn) {
+        String s = "death.attack.vampirism_dbno";
+        return new TranslatableComponent(s, this.originalSource);
+    }
+}
