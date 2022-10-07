727cc34762239fd051db4e73792b79c72cfbbad1
cheaterpaul <dev@paube.de>
2022-10-07 20:40:08 +0200

    add crossbow arrow selection screen

--- src/api/java/de/teamlapen/vampirism/api/items/IVampirismCrossbow.java
+++ src/api/java/de/teamlapen/vampirism/api/items/IVampirismCrossbow.java
@@ -1,16 +1,21 @@
 package de.teamlapen.vampirism.api.items;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * Implemented by any crossbow.
  * Used for crossbow enchantments
  */
-public interface IVampirismCrossbow extends ItemLike{
+public interface IVampirismCrossbow extends ItemLike {
 
     /**
      * modded variant of {@link net.minecraft.item.CrossbowItem#performShooting(net.minecraft.world.World, net.minecraft.entity.LivingEntity, net.minecraft.util.Hand, net.minecraft.item.ItemStack, float, float)} that is not static
@@ -26,4 +31,40 @@ public interface IVampirismCrossbow extends ItemLike{
      * @return duration in ticks
      */
     int getChargeDurationMod(ItemStack crossbow);
+
+    /**
+     * @param crossbow the crossbow
+     * @return true if the crossbow can select ammunition
+     */
+    boolean canSelectAmmunition(ItemStack crossbow);
+
+    /**
+     * @param crossbow
+     * @return the currently selected ammunition or {@link java.util.Optional#empty()} if no ammunition is selected
+     */
+    Optional<Item> getAmmunition(ItemStack crossbow);
+
+    /**
+     * sets the selected ammunition for the crossbow
+     *
+     * @param crossbow the crossbow
+     * @param ammo     the ammunition item
+     */
+    void setAmmunition(ItemStack crossbow, Item ammo);
+
+    /**
+     * sets the selected ammunition for the crossbow
+     *
+     * @param crossbow the crossbow
+     * @param ammo     the registry name if the ammunition item
+     */
+    void setAmmunition(ItemStack crossbow, ResourceLocation ammo);
+
+    /**
+     * gets a predicate to test all projectiles supported by the crossbow
+     *
+     * @param crossbow the crossbow
+     * @return a predicate that returns true if the given itemstack is a supported projectile
+     */
+    Predicate<ItemStack> getSupportedProjectiles(ItemStack crossbow);
 }
