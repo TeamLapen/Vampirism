90cf67836277d5bccf97841ed30c0e709f4302c4
cheaterpaul <dev@paube.de>
2022-10-12 21:39:46 +0200

    make effects version independent

--- src/components/Effects.tsx
+++ src/components/Effects.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-export const baseUrl = 'https://raw.githubusercontent.com/TeamLapen/Vampirism/'
-export const baseCommit = '4bf2c73fb860a23de225edbae9c0b1c1ead3dd1a'
-export const effectPath = '/src/main/resources/assets/vampirism/textures/mob_effect/'
-
-export const EffectImg = ({src, alt}) => (
+const EffectImg = ({src, alt}) => (
     <img src={src} alt={alt} style={{height: 70, imageRendering: "pixelated"}}/>
 );
 
-const EffectInternal = ({children, title, icon}) => (
+export const EffectInternal = ({children, title, icon}) => (
     <div style={{ marginBottom: '15px'}} id={title.toLowerCase().replaceAll(' ','-')}>
         <div className="container" style={{ display: 'flex', alignItems: 'center', marginLeft: '-20px'}}>
             <div className="image">
@@ -22,12 +18,4 @@ const EffectInternal = ({children, title, icon}) => (
             {children}
         </span>
     </div>
-);
-
-export const Effect = ({children, title, icon, commit=baseCommit}) => (
-    <EffectInternal title={title} icon={baseUrl.concat(commit).concat(effectPath).concat(icon)} children={children}/>
-);
-
-export const EffectC = ({children, title, icon}) => (
-    <EffectInternal title={title} children={children} icon={icon}/>
 );
