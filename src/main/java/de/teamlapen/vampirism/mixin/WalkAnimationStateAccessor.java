5628abbd359ade62bdc03f18ab0fb985b3790393
maxanier <dev@maxgb.de>
2023-03-24 17:54:54 +0100

    Fix converted creature walking animation

---
+++ src/main/java/de/teamlapen/vampirism/mixin/WalkAnimationStateAccessor.java
@@ -0,0 +1,24 @@
+package de.teamlapen.vampirism.mixin;
+
+import net.minecraft.world.entity.WalkAnimationState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(WalkAnimationState.class)
+public interface WalkAnimationStateAccessor {
+
+    @Accessor("speed")
+    float getSpeed();
+    @Accessor("speed")
+    void setSpeed(float speed);
+
+    @Accessor("speedOld")
+    float getSpeedOld();
+    @Accessor("speedOld")
+    void setSpeedOld(float speedOld);
+
+    @Accessor("position")
+    float getPosition();
+    @Accessor("position")
+    void setPosition(float position);
+}
