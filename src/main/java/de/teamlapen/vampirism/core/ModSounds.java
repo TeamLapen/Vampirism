68781980f4d2c16db69f912759398a09074d83cd
Max Becker <dev@maxgb.de>
2023-05-08 21:15:27 +0000

    Sound effects (#1198)
    * Reorganize sound files.
    Add some new sound effects
    
    * Finetune new  sounds
    
    * Subtitles

--- src/main/java/de/teamlapen/vampirism/core/ModSounds.java
+++ src/main/java/de/teamlapen/vampirism/core/ModSounds.java
@@ -15,18 +15,23 @@ import org.jetbrains.annotations.NotNull;
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, REFERENCE.MODID);
 
-    public static final RegistryObject<SoundEvent> ENTITY_VAMPIRE_SCREAM = create("entity_vampire_scream");
-    public static final RegistryObject<SoundEvent> PLAYER_BITE = create("player_bite");
-    public static final RegistryObject<SoundEvent> AMBIENT_CASTLE = create("ambient_castle");
+    public static final RegistryObject<SoundEvent> ENTITY_VAMPIRE_SCREAM = create("entity.vampire_scream");
+    public static final RegistryObject<SoundEvent> VAMPIRE_BITE = create("entity.vampire_bite");
+    public static final RegistryObject<SoundEvent> AMBIENT_BLOOD_DRIPPING = create("ambient.blood_dripping");
     public static final RegistryObject<SoundEvent> COFFIN_LID = create("coffin_lid");
-    public static final RegistryObject<SoundEvent> CROSSBOW = create("crossbow");
-    public static final RegistryObject<SoundEvent> BAT_SWARM = create("bat_swarm");
-    public static final RegistryObject<SoundEvent> BOILING = create("boiling");
-    public static final RegistryObject<SoundEvent> GRINDER = create("grinder");
+    public static final RegistryObject<SoundEvent> BAT_SWARM = create("entity.bat_swarm");
+    public static final RegistryObject<SoundEvent> BOILING = create("block.boiling");
+    public static final RegistryObject<SoundEvent> GRINDER = create("block.grinder");
     public static final RegistryObject<SoundEvent> TASK_COMPLETE = create("task_complete");
-    public static final RegistryObject<SoundEvent> PLAYER_FEEDING = create("player.feeding");
-    public static final RegistryObject<SoundEvent> BLESSING_MUSIC = create("blessing_music");
-
+    public static final RegistryObject<SoundEvent> VAMPIRE_FEEDING = create("entity.vampire_feeding");
+    public static final RegistryObject<SoundEvent> BLESSING_MUSIC = create("block.blessing_music");
+    public static final RegistryObject<SoundEvent> BLOOD_PROJECTILE_HIT  = create("fx.blood_projectile_hit");
+    public static final RegistryObject<SoundEvent> WEAPON_TABLE_CRAFTING = create("block.weapon_table_crafting");
+    public static final RegistryObject<SoundEvent> STAKE = create("fx.stake");
+    public static final RegistryObject<SoundEvent> TELEPORT_AWAY = create("fx.teleport_away");
+    public static final RegistryObject<SoundEvent> TELEPORT_HERE = create("fx.teleport_here");
+    public static final RegistryObject<SoundEvent> FREEZE = create("fx.freeze");
+    public static final RegistryObject<SoundEvent> POTION_TABLE_CRAFTING = create("block.potion_table_crafting");
 
     static void register(IEventBus bus) {
         SOUND_EVENTS.register(bus);
--- src/main/java/de/teamlapen/vampirism/entity/player/vampire/actions/FreezeVampireAction.java
+++ src/main/java/de/teamlapen/vampirism/entity/player/vampire/actions/FreezeVampireAction.java
@@ -6,10 +6,12 @@ import de.teamlapen.vampirism.config.VampirismConfig;
 import de.teamlapen.vampirism.core.ModEffects;
 import de.teamlapen.vampirism.core.ModParticles;
 import de.teamlapen.vampirism.core.ModRefinements;
+import de.teamlapen.vampirism.core.ModSounds;
 import de.teamlapen.vampirism.entity.BlindingBatEntity;
 import de.teamlapen.vampirism.entity.player.VampirismPlayerAttributes;
 import de.teamlapen.vampirism.particle.GenericParticleOptions;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -41,6 +43,7 @@ public class FreezeVampireAction extends DefaultVampireAction {
                 dur *= VampirismConfig.BALANCE.vrFreezeDurationMod.get();
             }
             e.addEffect(new MobEffectInstance(ModEffects.FREEZE.get(), dur));
+            e.getCommandSenderWorld().playSound(null, e.getX(), e.getY(), e.getZ(), ModSounds.FREEZE.get(), SoundSource.PLAYERS, 1f, 1f);
             ModParticles.spawnParticlesServer(player.getCommandSenderWorld(), new GenericParticleOptions(new ResourceLocation("minecraft", "generic_2"), 20, 0xF0F0F0, 0.4F), e.getX(), e.getY(), e.getZ(), 20, 1, 1, 1, 0);
         }
         return l.size() > 0;
