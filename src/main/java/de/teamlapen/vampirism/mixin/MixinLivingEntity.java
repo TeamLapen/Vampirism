aa1928ba02348187c9986e068f70e723765afa0d
maxanier <dev@maxgb.de>
2023-08-05 13:12:13 +0200

    Add mixin for MobType

--- src/main/java/de/teamlapen/vampirism/mixin/MixinLivingEntity.java
+++ src/main/java/de/teamlapen/vampirism/mixin/MixinLivingEntity.java
@@ -7,6 +7,8 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,18 +19,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
-    private MixinLivingEntity(@NotNull EntityType<? extends LivingEntity> type, @NotNull Level worldIn) {
+    protected MixinLivingEntity(@NotNull EntityType<? extends LivingEntity> type, @NotNull Level worldIn) {
         super(type, worldIn);
     }
 
     @Shadow
     public abstract boolean addEffect(MobEffectInstance effectInstanceIn);
 
+    @Shadow
+    public abstract ItemStack getMainHandItem(); // Needed in subclass
+
+
     @Inject(method = "checkTotemDeathProtection", at = @At("RETURN"))
     private void handleTotemOfUndying(DamageSource damageSourceIn, @NotNull CallbackInfoReturnable<Boolean> cir) {
         if (cir.getReturnValue() && Helper.isVampire(this)) {
             this.addEffect(new MobEffectInstance(ModEffects.FIRE_PROTECTION.get(), 800, 5));
             this.addEffect(new MobEffectInstance(ModEffects.SUNSCREEN.get(), 800, 4));
+        }
+    }
+
+    protected MobType vampirism$getMobType() {
+        return null;
+    }
+
+    @Inject(method = "getMobType", at = @At("HEAD"), cancellable = true)
+    private void handleGetMobType(CallbackInfoReturnable<MobType> cir) {
+        MobType t = vampirism$getMobType();
+        if (t != null) {
+            cir.setReturnValue(t);
+            cir.cancel();
         }
     }
 }
--- src/main/java/de/teamlapen/vampirism/mixin/MixinPlayerEntity.java
+++ src/main/java/de/teamlapen/vampirism/mixin/MixinPlayerEntity.java
@@ -1,11 +1,13 @@
 package de.teamlapen.vampirism.mixin;
 
+import de.teamlapen.vampirism.config.VampirismConfig;
 import de.teamlapen.vampirism.entity.player.IVampirismPlayer;
 import de.teamlapen.vampirism.entity.player.VampirismPlayerAttributes;
 import de.teamlapen.vampirism.util.MixinHooks;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
@@ -14,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Player.class)
-public abstract class MixinPlayerEntity extends LivingEntity implements IVampirismPlayer {
+public abstract class MixinPlayerEntity extends MixinLivingEntity implements IVampirismPlayer {
 
     private final VampirismPlayerAttributes vampirismPlayerAttributes = new VampirismPlayerAttributes();
 
@@ -30,5 +32,13 @@ public abstract class MixinPlayerEntity extends LivingEntity implements IVampiri
     @ModifyVariable(method = "attack(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "STORE", ordinal = 0), ordinal = 1)
     public float vampireSlayerEnchantment(float damage, Entity target) {
         return damage + MixinHooks.calculateVampireSlayerEnchantments(target, this.getMainHandItem());
+    }
+
+    @Override
+    protected MobType vampirism$getMobType() {
+        if (getVampAtts().vampireLevel > 0 && VampirismConfig.SERVER.vampiresAreUndeadType.get()) {
+            return MobType.UNDEAD;
+        }
+        return null;
     }
 }
