75516db2899c13d89b2a2beb3cbb97407f6da8c4
cheaterpaul <dev@paube.de>
2024-02-06 14:41:50 +0100

    add helmet to hunter representatives

--- src/main/java/de/teamlapen/vampirism/client/renderer/entity/HunterTaskMasterRenderer.java
+++ src/main/java/de/teamlapen/vampirism/client/renderer/entity/HunterTaskMasterRenderer.java
@@ -4,14 +4,30 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import de.teamlapen.vampirism.REFERENCE;
 import de.teamlapen.vampirism.client.core.ModEntitiesRender;
 import de.teamlapen.vampirism.client.renderer.entity.layers.TaskMasterTypeLayer;
+import de.teamlapen.vampirism.entity.hunter.AdvancedHunterEntity;
 import de.teamlapen.vampirism.entity.hunter.HunterTaskMasterEntity;
 import de.teamlapen.vampirism.mixin.client.VillagerModelAccessor;
+import net.minecraft.client.model.AgeableListModel;
+import net.minecraft.client.model.HumanoidArmorModel;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.VillagerModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.armortrim.ArmorTrim;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
@@ -28,6 +44,7 @@ public class HunterTaskMasterRenderer extends MobRenderer<HunterTaskMasterEntity
         super(context, new VillagerModel<>(context.bakeLayer(ModEntitiesRender.TASK_MASTER)), 0.5F);
 //        this.addLayer(new HeldItemLayer<>(this));
         this.addLayer(new TaskMasterTypeLayer<>(this, overlay));
+        this.addLayer(new HelmetLayer(this, context.getItemInHandRenderer()));
     }
 
     @NotNull
@@ -41,6 +58,29 @@ public class HunterTaskMasterRenderer extends MobRenderer<HunterTaskMasterEntity
         double dist = this.entityRenderDispatcher.distanceToSqr(entityIn);
         if (dist <= 128) {
             super.renderNameTag(entityIn, displayNameIn, matrixStackIn, bufferIn, packedLightIn);
+        }
+    }
+
+    private static class HelmetLayer extends RenderLayer<HunterTaskMasterEntity, VillagerModel<HunterTaskMasterEntity>> {
+        private final ItemInHandRenderer pItemInHandRenderer;
+
+        public HelmetLayer(RenderLayerParent<HunterTaskMasterEntity, VillagerModel<HunterTaskMasterEntity>> pRenderer, ItemInHandRenderer pItemInHandRenderer) {
+            super(pRenderer);
+            this.pItemInHandRenderer = pItemInHandRenderer;
+        }
+
+        @Override
+        public void render(@NotNull PoseStack pPoseStack, @NotNull MultiBufferSource pBuffer, int pPackedLight, HunterTaskMasterEntity pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pPartialTick, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+            ItemStack itemstack = pLivingEntity.getItemBySlot(EquipmentSlot.HEAD);
+            if (!itemstack.isEmpty()) {
+                pPoseStack.pushPose();
+                this.getParentModel().getHead().translateAndRotate(pPoseStack);
+                CustomHeadLayer.translateToHead(pPoseStack, true);
+                pPoseStack.translate(0.0F, -0.2F, 0.0F);
+                pPoseStack.scale(1.1F, 1.1F, 1.1F);
+                this.pItemInHandRenderer.renderItem(pLivingEntity, itemstack, ItemDisplayContext.HEAD, false, pPoseStack, pBuffer, pPackedLight);
+                pPoseStack.popPose();
+            }
         }
     }
 
