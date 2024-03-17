297b6e4764755bd59da543a20b515de6b85629f9
cheaterpaul <dev@paube.de>
2024-03-17 13:51:47 +0100

    make ItemCombinerMenu#inputSlots mutable
    close #1347

--- src/main/java/de/teamlapen/vampirism/mixin/accessor/ItemCombinerMenuAccessor.java
+++ src/main/java/de/teamlapen/vampirism/mixin/accessor/ItemCombinerMenuAccessor.java
@@ -2,7 +2,9 @@ package de.teamlapen.vampirism.mixin.accessor;
 
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.ItemCombinerMenu;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ItemCombinerMenu.class)
@@ -12,5 +14,6 @@ public interface ItemCombinerMenuAccessor {
     Container getInputSlots();
 
     @Accessor("inputSlots")
+    @Mutable
     void setInputSlots(Container inputSlots);
 }
