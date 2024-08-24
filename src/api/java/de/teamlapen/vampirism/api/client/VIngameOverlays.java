c92d7340102d6c235c21faf4f0d4886e2f3f7194
cheaterpaul <dev@paube.de>
2024-08-24 18:41:06 +0200

    remove deprecated fields in VIngameOverlays

--- src/api/java/de/teamlapen/vampirism/api/client/VIngameOverlays.java
+++ src/api/java/de/teamlapen/vampirism/api/client/VIngameOverlays.java
@@ -1,7 +1,6 @@
 package de.teamlapen.vampirism.api.client;
 
 import de.teamlapen.vampirism.api.util.VResourceLocation;
-import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.resources.ResourceLocation;
 
 /**
@@ -14,8 +13,6 @@ public class VIngameOverlays {
      * <br>
      * Is rendered above {@link net.neoforged.neoforge.client.gui.VanillaGuiLayers#FOOD_LEVEL}, but the food rendering is canceled
      */
-    @Deprecated(forRemoval = true)
-    public static LayeredDraw.Layer BLOOD_BAR_ELEMENT;
     public static final ResourceLocation BLOOD_BAR_ID = VResourceLocation.mod("blood_bar");
 
 
@@ -24,8 +21,6 @@ public class VIngameOverlays {
      * <br>
      * Is rendered above {@link net.neoforged.neoforge.client.gui.VanillaGuiLayers#BOSS_OVERLAY}
      */
-    @Deprecated(forRemoval = true)
-    public static LayeredDraw.Layer FACTION_RAID_BAR_ELEMENT;
     public static final ResourceLocation FACTION_RAID_BAR_ID = VResourceLocation.mod("raid_bar");
 
 
@@ -34,8 +29,6 @@ public class VIngameOverlays {
      * <br>
      * Is rendered above {@link net.neoforged.neoforge.client.gui.VanillaGuiLayers#EXPERIENCE_BAR}
      */
-    @Deprecated(forRemoval = true)
-    public static LayeredDraw.Layer FACTION_LEVEL_ELEMENT;
     public static final ResourceLocation FACTION_LEVEL_ID = VResourceLocation.mod("faction_level");
 
     /**
@@ -43,8 +36,6 @@ public class VIngameOverlays {
      * <br>
      * Is rendered in the lower left corner
      */
-    @Deprecated(forRemoval = true)
-    public static LayeredDraw.Layer ACTION_COOLDOWN_ELEMENT;
     public static final ResourceLocation ACTION_COOLDOWN_ID = VResourceLocation.mod("action_cooldown");
 
     /**
@@ -52,8 +43,6 @@ public class VIngameOverlays {
      * <br>
      * Is rendered in the lower right corner
      */
-    @Deprecated(forRemoval = true)
-    public static LayeredDraw.Layer ACTION_DURATION_ELEMENT;
     public static final ResourceLocation ACTION_DURATION_ID = VResourceLocation.mod("action_duration");
     public static final ResourceLocation RAGE = VResourceLocation.mod("rage");
     public static final ResourceLocation BAT = VResourceLocation.mod("bat");
