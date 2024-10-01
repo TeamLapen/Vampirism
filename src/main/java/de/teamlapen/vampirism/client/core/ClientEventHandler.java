1226c5a57eeca235c6f2869e237ca30aeb638584
cheaterpaul <dev@paube.de>
2024-10-01 15:11:15 +0200

    make applied oil tooltip on weapons slightly less noticeable

--- src/main/java/de/teamlapen/vampirism/client/core/ClientEventHandler.java
+++ src/main/java/de/teamlapen/vampirism/client/core/ClientEventHandler.java
@@ -179,7 +179,7 @@ public class ClientEventHandler {
             }
             List<Component> factionToolTips = new ArrayList<>();
             factionToolTips.add(Component.empty());
-            factionToolTips.add(Component.translatable("text.vampirism.faction_exclusive", ModFactions.HUNTER.get().getName().copy().withStyle(Minecraft.getInstance().player != null ? Helper.isHunter(Minecraft.getInstance().player) ? ChatFormatting.DARK_GREEN : ChatFormatting.DARK_RED : ChatFormatting.GRAY)));
+            factionToolTips.add(Component.translatable("text.vampirism.faction_exclusive", ModFactions.HUNTER.get().getName().plainCopy().withStyle(Minecraft.getInstance().player != null ? Helper.isHunter(Minecraft.getInstance().player) ? ChatFormatting.DARK_GREEN : ChatFormatting.DARK_RED : ChatFormatting.GRAY)).withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GRAY));
             toolTips.addAll(Math.min(event.getToolTip().size(), position), factionToolTips);
         });
     }
