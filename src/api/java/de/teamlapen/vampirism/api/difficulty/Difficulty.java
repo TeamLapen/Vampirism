6a1613580905c784d58b2687fc8118eca9262780
maxanier <dev@maxgb.de>
2024-10-05 13:54:48 +0200

    Fix difficulty system being broken.
    and: f50ecbadb8b0eeb011b56bc13fc7f5baef54baa8
    Fix potential crash with VampireBaron #1428
    Improve Difficulty system documentation

--- src/api/java/de/teamlapen/vampirism/api/difficulty/Difficulty.java
+++ src/api/java/de/teamlapen/vampirism/api/difficulty/Difficulty.java
@@ -1,14 +1,48 @@
 package de.teamlapen.vampirism.api.difficulty;
 
+import org.jetbrains.annotations.NotNull;
+
+import static net.minecraft.util.Mth.clamp;
+
 /**
  * Represents a calculated difficulty level.
  * The used difficulty levels are in percentage of the max reachable level.
  * So a level 5 in a faction which has a max level of 10 would be represented by 5/10*100=50
  */
-public record Difficulty(int minPercLevel, int maxPercLevel, int avgPercLevel) {
+public class Difficulty {
+
+    /**
+     * Percentage between 0 and 100
+     */
+    public final int minPercLevel, maxPercLevel, avgPercLevel;
+
+    public Difficulty(int mil, int mal, int al) {
+        mal = clamp(mal, 0, 100);
+        mil = clamp(mil, 0, mal);
+        al = clamp(al, mil, mal);
+        this.minPercLevel = mil;
+        this.maxPercLevel = mal;
+        this.avgPercLevel = al;
+    }
+
+    public int minPercLevel() {
+        return minPercLevel;
+    }
+
+    public int maxPercLevel() {
+        return maxPercLevel;
+    }
+
+    public int avgPercLevel() {
+        return avgPercLevel;
+    }
 
     public boolean isZero() {
         return (this.minPercLevel == 0 && this.maxPercLevel == 0);
     }
 
+    @Override
+    public @NotNull String toString() {
+        return "Difficulty: min_" + minPercLevel + " max_" + maxPercLevel + " avg_" + avgPercLevel;
+    }
 }
