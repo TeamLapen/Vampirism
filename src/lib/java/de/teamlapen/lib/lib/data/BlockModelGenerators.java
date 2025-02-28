47ab62842b63a01b16541c1336908885e104bbd8
cheaterpaul <dev@paube.de>
2025-02-28 23:50:15 +0100

    fix alchemical fire rendertype

--- src/lib/java/de/teamlapen/lib/lib/data/BlockModelGenerators.java
+++ src/lib/java/de/teamlapen/lib/lib/data/BlockModelGenerators.java
@@ -1,5 +1,6 @@
 package de.teamlapen.lib.lib.data;
 
+import com.google.common.collect.ImmutableList;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.client.data.models.ItemModelOutput;
@@ -16,15 +17,26 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public abstract class BlockModelGenerators extends net.minecraft.client.data.models.BlockModelGenerators {
-    private static final ModelTemplate CROP = copy(net.minecraft.client.data.models.model.ModelTemplates.CROP, ResourceLocation.withDefaultNamespace("cutout"));
+    protected static final ResourceLocation CUTOUT = ResourceLocation.withDefaultNamespace("cutout");
+    protected static final ModelTemplate CROP = copy(ModelTemplates.CROP);
+    protected static final ModelTemplate FIRE_FLOOR = copy(ModelTemplates.FIRE_FLOOR);
+    protected static final ModelTemplate FIRE_SIDE = copy(ModelTemplates.FIRE_SIDE);
+    protected static final ModelTemplate FIRE_SIDE_ALT = copy(ModelTemplates.FIRE_SIDE_ALT);
+    protected static final ModelTemplate FIRE_UP = copy(ModelTemplates.FIRE_UP);
+    protected static final ModelTemplate FIRE_UP_ALT = copy(ModelTemplates.FIRE_UP_ALT);
 
-    private static ModelTemplate copy(ModelTemplate template, ResourceLocation renderType) {
+    protected static ModelTemplate copy(ModelTemplate template) {
+        return copy(template, CUTOUT);
+    }
+
+    protected static ModelTemplate copy(ModelTemplate template, ResourceLocation renderType) {
         return template.extend().renderType(renderType).build();
     }
 
@@ -108,5 +120,41 @@ public abstract class BlockModelGenerators extends net.minecraft.client.data.mod
             this.registerSimpleFlatItemModel(cropBlock.asItem());
             this.blockStateOutput.accept(MultiVariantGenerator.multiVariant(cropBlock).with(propertydispatch));
         }
+    }
+
+
+    @Override
+    public @NotNull List<ResourceLocation> createFloorFireModels(@NotNull Block fireBlock) {
+        ResourceLocation resourcelocation = FIRE_FLOOR
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_floor0"), TextureMapping.fire0(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation1 = FIRE_FLOOR
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_floor1"), TextureMapping.fire1(fireBlock), this.modelOutput);
+        return ImmutableList.of(resourcelocation, resourcelocation1);
+    }
+
+    @Override
+    public @NotNull List<ResourceLocation> createSideFireModels(@NotNull Block fireBlock) {
+        ResourceLocation resourcelocation = FIRE_SIDE
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_side0"), TextureMapping.fire0(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation1 = FIRE_SIDE
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_side1"), TextureMapping.fire1(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation2 = FIRE_SIDE_ALT
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_side_alt0"), TextureMapping.fire0(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation3 = FIRE_SIDE_ALT
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_side_alt1"), TextureMapping.fire1(fireBlock), this.modelOutput);
+        return ImmutableList.of(resourcelocation, resourcelocation1, resourcelocation2, resourcelocation3);
+    }
+
+    @Override
+    public @NotNull List<ResourceLocation> createTopFireModels(@NotNull Block fireBlock) {
+        ResourceLocation resourcelocation = FIRE_UP
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_up0"), TextureMapping.fire0(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation1 = FIRE_UP
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_up1"), TextureMapping.fire1(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation2 = FIRE_UP_ALT
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_up_alt0"), TextureMapping.fire0(fireBlock), this.modelOutput);
+        ResourceLocation resourcelocation3 = FIRE_UP_ALT
+                .create(ModelLocationUtils.getModelLocation(fireBlock, "_up_alt1"), TextureMapping.fire1(fireBlock), this.modelOutput);
+        return ImmutableList.of(resourcelocation, resourcelocation1, resourcelocation2, resourcelocation3);
     }
 }
--- src/main/java/de/teamlapen/vampirism/data/provider/model/BlockModelGenerators.java
+++ src/main/java/de/teamlapen/vampirism/data/provider/model/BlockModelGenerators.java
@@ -308,24 +308,14 @@ public class BlockModelGenerators extends de.teamlapen.lib.lib.data.BlockModelGe
     protected void createAlchemicalFire() {
         List<ResourceLocation> list = this.createFloorFireModels(ModBlocks.ALCHEMICAL_FIRE.get());
         List<ResourceLocation> list1 = this.createSideFireModels(ModBlocks.ALCHEMICAL_FIRE.get());
-        List<ResourceLocation> list2 = this.createTopFireModels(ModBlocks.ALCHEMICAL_FIRE.get());
         this.blockStateOutput
                 .accept(
                         MultiPartGenerator.multiPart(ModBlocks.ALCHEMICAL_FIRE.get())
                                 .with(wrapModels(list, p_387290_ -> p_387290_))
                                 .with(wrapModels(list1, p_386602_ -> p_386602_))
-                                .with(
-                                        wrapModels(list1, p_388679_ -> p_388679_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90))
-                                )
-                                .with(
-
-                                        wrapModels(list1, p_387956_ -> p_387956_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R180))
-                                )
-                                .with(
-
-                                        wrapModels(list1, p_386620_ -> p_386620_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270))
-                                )
-                                .with(wrapModels(list2, p_387194_ -> p_387194_))
+                                .with(wrapModels(list1, p_388679_ -> p_388679_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90)))
+                                .with(wrapModels(list1, p_387956_ -> p_387956_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R180)))
+                                .with(wrapModels(list1, p_386620_ -> p_386620_.with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270)))
                 );
     }
 
@@ -446,5 +436,6 @@ public class BlockModelGenerators extends de.teamlapen.lib.lib.data.BlockModelGe
                 .with(Condition.condition().term(BloodInfuserBlock.IS_ACTIVE, true), Variant.variant().with(VariantProperties.MODEL, VResourceLocation.mod("block/blood_infuser/infuser_blood"))));
         this.createDefaultBlockItem(ModBlocks.INFUSER.get(), VResourceLocation.mod("block/blood_infuser/infuser"));
     }
+
 
 }
