09352f59f7c596a4b9fad5a49f82ca314a034428
cheaterpaul <dev@paube.de>
2025-03-04 22:17:02 +0100

    integrate action statistics correctly into the layout
    close #1476

--- src/main/java/de/teamlapen/vampirism/mixin/client/StatsScreenMixin.java
+++ src/main/java/de/teamlapen/vampirism/mixin/client/StatsScreenMixin.java
@@ -1,8 +1,12 @@
 package de.teamlapen.vampirism.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import de.teamlapen.vampirism.client.gui.components.ActionStatisticsList;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.ObjectSelectionList;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.achievement.StatsScreen;
 import net.minecraft.network.chat.Component;
@@ -31,9 +35,14 @@ public abstract class StatsScreenMixin extends Screen {
         this.vampirism$actionStatisticsList = new ActionStatisticsList(this.minecraft, ((StatsScreen) (Object)this), width, height-96);
     }
 
-    @Inject(method = "initButtons", at = @At("RETURN"))
-    private void initButtons(CallbackInfo ci) {
-        var button = this.addRenderableWidget(Button.builder(Component.translatable("text.vampirism.actions"), b -> this.setActiveList(this.vampirism$actionStatisticsList)).bounds(this.width / 2 + 120, this.height -52, 80, 20).build());
+    @Inject(method = "initButtons", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/LinearLayout;addChild(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;", ordinal = 3, shift = At.Shift.AFTER))
+    private void add(CallbackInfo ci, @Local(ordinal = 1) LinearLayout line) {
+        line.visitChildren(s -> {
+            if (s instanceof Button button) {
+                button.setWidth(100);
+            }
+        });
+        var button = line.addChild(Button.builder(Component.translatable("text.vampirism.actions"), x -> this.setActiveList(this.vampirism$actionStatisticsList)).width(100).build());
         if (this.vampirism$actionStatisticsList.children().isEmpty()) {
             button.active = false;
         }
