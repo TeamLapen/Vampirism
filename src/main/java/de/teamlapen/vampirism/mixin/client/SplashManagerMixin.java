9c05c4c4f9100e8377e992733af29c7a3e1cfdf3
TheGridExpert <grid.write.here@gmail.com>
2025-06-20 13:23:35 +0300

    Make the mod's splashes more random

--- src/main/java/de/teamlapen/vampirism/mixin/client/SplashManagerMixin.java
+++ src/main/java/de/teamlapen/vampirism/mixin/client/SplashManagerMixin.java
@@ -5,6 +5,7 @@ import de.teamlapen.vampirism.VampirismMod;
 import net.minecraft.client.gui.components.SplashRenderer;
 import net.minecraft.client.resources.SplashManager;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -28,37 +30,47 @@ public class SplashManagerMixin {
 
     @Unique
     private static final String VAMPIRISM_SPLASHES_LOCATION = "/assets/" + REFERENCE.MODID + "/texts/splashes.txt";
-
     @Unique
     private static final Logger vampirism$LOGGER = LogManager.getLogger();
+    @Unique
+    private static final RandomSource vampirism$RANDOM = RandomSource.create();
 
     @Final
     @Shadow
     private List<String> splashes;
+    @Unique
+    private List<String> vampirism$customSplashes = Collections.emptyList();
 
-    @Inject(method = "apply(Ljava/util/List;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("TAIL"))
-    private void applyCustomSplashes(List<String> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
+    @Inject(method = "prepare(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)Ljava/util/List;", at = @At("RETURN"))
+    private void vampirism$prepare(ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfoReturnable<List<String>> cir) {
         try (InputStream inputStream = VampirismMod.class.getResourceAsStream(VAMPIRISM_SPLASHES_LOCATION)) {
             if (inputStream != null) {
-                try (Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
-                    BufferedReader bufferedReader = new BufferedReader(reader);
-                    List<String> customSplashes = bufferedReader.lines().map(String::trim).filter(line -> !line.isEmpty()).toList();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    vampirism$customSplashes = reader.lines().map(String::trim).filter(line -> !line.isEmpty()).toList();
 
-                    // Vampirism's splashes are added more than one time in order to increase their chance of being displayed, otherwise they would unlikely be rolled at all
-                    double chance = 0.2;
-                    int timesAdded = (int) Math.ceil((chance * splashes.size()) / ((1 - chance) * customSplashes.size()));
-                    for (int i = 0; i < timesAdded; i++) {
-                        this.splashes.addAll(customSplashes);
-                    }
-
-                    vampirism$LOGGER.info("Successfully loaded and added {} Vampirism splashes {} times to {} vanilla ones", customSplashes.size(), timesAdded, splashes.size() - customSplashes.size() * timesAdded);
-                } catch (Exception exception) {
-                    vampirism$LOGGER.warn("Failed to load Vampirism splash file", exception);
+                    vampirism$LOGGER.info("Loaded {} Vampirism splashes", vampirism$customSplashes.size());
                 }
             }
-        } catch (IOException exception) {
-            vampirism$LOGGER.warn("Failed to load Vampirism splashes", exception);
+        } catch (Exception e) {
+            vampirism$LOGGER.warn("Failed to load Vampirism splash file", e);
         }
+    }
+
+    @Inject(method = "apply(Ljava/util/List;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("TAIL"))
+    private void vampirism$apply(List<String> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
+        if (vampirism$customSplashes.isEmpty()) return;
+
+        double chance = 0.2;
+        int originalSize = splashes.size();
+        int timesAdded = (int) Math.ceil((chance * originalSize) / ((1 - chance) * vampirism$customSplashes.size()));
+
+        for (int i = 0; i < timesAdded; i++) {
+            for (String splash : vampirism$customSplashes) {
+                splashes.add(vampirism$RANDOM.nextInt(splashes.size() + 1), splash);
+            }
+        }
+
+        vampirism$LOGGER.info("Successfully loaded and added {} Vampirism splashes {} times each. Final list size: {}", vampirism$customSplashes.size(), timesAdded, this.splashes.size());
     }
 
     @Inject(method = "getSplash", at = @At("HEAD"), cancellable = true)
@@ -67,6 +79,8 @@ public class SplashManagerMixin {
         calendar.setTime(new Date());
         if (calendar.get(Calendar.MONTH) == Calendar.AUGUST && calendar.get(Calendar.DATE) == 11) {
             cir.setReturnValue(new SplashRenderer("Happy anniversary, Vampirism!"));
+        } else if (calendar.get(Calendar.MONTH) == Calendar.NOVEMBER && calendar.get(Calendar.DATE) == 16) {
+            cir.setReturnValue(new SplashRenderer("Happy anniversary, Vampire's Delight!"));
         }
     }
 }
