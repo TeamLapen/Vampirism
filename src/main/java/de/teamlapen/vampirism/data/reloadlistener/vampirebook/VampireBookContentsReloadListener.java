ab87f26b96d00943ccd8cddae48eee986e455747
TheGridExpert <grid.write.here@gmail.com>
2025-07-22 23:24:15 +0300

    Move vampire book localization combination logic to the prepare method

--- src/main/java/de/teamlapen/vampirism/data/reloadlistener/vampirebook/VampireBookContentsReloadListener.java
+++ src/main/java/de/teamlapen/vampirism/data/reloadlistener/vampirebook/VampireBookContentsReloadListener.java
@@ -19,6 +19,7 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -40,23 +41,53 @@ public class VampireBookContentsReloadListener extends SimplePreparableReloadLis
             return parts[1];
         })));
 
-        return contentByBook.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, x -> new TranslatedBookContent(x.getValue().stream().<Pair<String, IBookContents>>mapMulti((id, resources) -> {
-            Resource resource = vampireBooks.get(id);
+        return contentByBook.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, x -> {
+            Map<String, IBookContents> rawLanguageMap = x.getValue().stream().<Pair<String, IBookContents>>mapMulti((id, resources) -> {
+                Resource resource = vampireBooks.get(id);
 
-            String[] split = id.getPath().split("/");
-            if (split.length != 3) {
-                return;
+                String[] split = id.getPath().split("/");
+                if (split.length != 3) {
+                    return;
+                }
+
+                try (var open = resource.openAsReader()) {
+                    var lang = split[2].split("\\.")[0];
+
+                    IBookContents content = BookContents.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseReader(open)).getOrThrow(DecoderException::new);
+                    resources.accept(Pair.of(lang, content));
+                } catch (Exception e) {
+                    LOGGER.error("Could not read vampire book contents file {} from {}", id, resource.sourcePackId(), e);
+                }
+            }).collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+
+            IBookContents base = rawLanguageMap.get("en_us");
+            Map<String, IBookContents> mergedLanguageMap = new HashMap<>();
+
+            for (Map.Entry<String, IBookContents> entry : rawLanguageMap.entrySet()) {
+                String lang = entry.getKey();
+                IBookContents localized = entry.getValue();
+
+                if (lang.equals("en_us") && base != null) {
+                    mergedLanguageMap.put(lang, base);
+                    continue;
+                }
+
+                if (base != null) {
+                    ResourceLocation background = !localized.background().equals(ModVampireBooks.DIARY_BACKGROUND) ? localized.background() : base.background();
+
+                    Map<Integer, IBookContents.IImageEntry> baseImages = base.images().stream().collect(Collectors.toMap(IBookContents.IImageEntry::id, image -> image));
+                    for (IBookContents.IImageEntry localizedImage : localized.images()) {
+                        baseImages.put(localizedImage.id(), localizedImage);
+                    }
+
+                    mergedLanguageMap.put(lang, new BookContents(localized.contents(), background, baseImages.values().stream().toList()));
+                } else {
+                    mergedLanguageMap.put(lang, localized);
+                }
             }
 
-            try (var open = resource.openAsReader()) {
-                var lang = split[2].split("\\.")[0];
-
-                IBookContents content = BookContents.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseReader(open)).getOrThrow(DecoderException::new);
-                resources.accept(Pair.of(lang, content));
-            } catch (Exception e) {
-                LOGGER.error("Could not read vampire book contents file {} from {}", id, resource.sourcePackId(), e);
-            }
-        }).collect(Collectors.toMap(Pair::getKey, Pair::getValue)))));
+            return new TranslatedBookContent(mergedLanguageMap);
+        }));
     }
 
     @Override
@@ -78,20 +109,7 @@ public class VampireBookContentsReloadListener extends SimplePreparableReloadLis
 
         @Nullable
         public IBookContents getContentsFor(String languageCode) {
-            IBookContents base = contents.get("en_us");
-            if (languageCode.equals("en_us") || base == null) return base;
-
-            IBookContents localized = contents.get(languageCode);
-            if (localized == null) return base;
-
-            ResourceLocation background = !localized.background().equals(ModVampireBooks.DIARY_BACKGROUND) ? localized.background() : base.background();
-
-            Map<Integer, IBookContents.IImageEntry> baseImages = base.images().stream().collect(Collectors.toMap(IBookContents.IImageEntry::id, image -> image));
-            for (IBookContents.IImageEntry localizedImage : localized.images()) {
-                baseImages.put(localizedImage.id(), localizedImage);
-            }
-
-            return new BookContents(localized.contents(), background, baseImages.values().stream().toList());
+            return contents.getOrDefault(languageCode, contents.get("en_us"));
         }
     }
 }
