0f8c56775588d1d2eeac4d9d4ff036c8b883382e
TheGridExpert <grid.write.here@gmail.com>
2026-04-05 23:18:57 +0300

    Make the garlic injection and the bat cage use the new entity interaction method

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/common/world/items/ItemEventHandler.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/common/world/items/ItemEventHandler.java
@@ -1,22 +1,12 @@
 package de.teamlapen.vampirism.common.world.items;
 
-import de.teamlapen.vampirism.common.core.ModItems;
-import de.teamlapen.vampirism.common.core.ModSounds;
-import de.teamlapen.vampirism.common.world.entity.ExtendedCreature;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.ItemAttributeModifierEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 public class ItemEventHandler {
 
@@ -28,44 +18,6 @@ public class ItemEventHandler {
             event.addModifier(Attributes.ATTACK_SPEED, new AttributeModifier(BuiltInRegistries.ITEM.getKey(sword), sword.getSpeedModifier(stack), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
             event.addModifier(Attributes.ARMOR_TOUGHNESS, new AttributeModifier(BuiltInRegistries.ITEM.getKey(sword).withSuffix("_purity"), sword.getPurityArmorToughnessModifier(stack) , AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
             event.addModifier(Attributes.ENTITY_INTERACTION_RANGE, new AttributeModifier(BuiltInRegistries.ITEM.getKey(sword).withSuffix("_purity"), sword.getPurityInteractionRangeModifier(stack) , AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND);
-        }
-    }
-
-    @SubscribeEvent
-    public void onSyringeMobInteraction(PlayerInteractEvent.EntityInteract event) {
-        Player player = event.getEntity();
-        Level level = event.getLevel();
-        ItemStack heldStack = event.getItemStack();
-        Entity target = event.getTarget();
-
-        if (!(target instanceof LivingEntity)) return;
-
-        if (heldStack.is(ModItems.INJECTION_GARLIC)) {
-            event.setCanceled(true);
-            event.setCancellationResult(InteractionResult.SUCCESS);
-
-            if (level.isClientSide()) return;
-
-            ExtendedCreature.getSafe(target).ifPresent(entity -> entity.setPoisonousBlood(ExtendedCreature.POISONOUS_BLOOD_DOSE_DURATION));
-
-            if (!player.isCreative()) {
-                if (heldStack.getCount() == 1) {
-                    player.setItemInHand(event.getHand(), heldStack.getCraftingRemainder());
-                } else {
-                    heldStack.shrink(1);
-                    player.addItem(heldStack.getCraftingRemainder());
-                }
-            }
-
-            // TODO: Find some other sound for vaccinating mobs
-            level.playSound(null, player.blockPosition(), ModSounds.VAMPIRE_BITE.get(), SoundSource.PLAYERS, 1.0f,  1.0f);
-        }
-    }
-
-    @SubscribeEvent
-    public void handleEntityInteractableItems(PlayerInteractEvent.EntityInteract event) {
-        if (event.getItemStack().getItem() instanceof IEntityInteractable interactable) {
-            interactable.onEntityInteract(event.getItemStack(), event.getTarget(), event.getEntity(), event.getLevel(), event.getHand());
         }
     }
 }
