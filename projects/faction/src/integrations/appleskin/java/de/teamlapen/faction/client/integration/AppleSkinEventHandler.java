9784e9a788c428834c80b9306ac2993edf6bda64
TheGridExpert <grid.write.here@gmail.com>
2026-04-26 15:44:06 +0300

    Change the injectFactionFoodValues mixin into an event

---
+++ projects/faction/src/integrations/appleskin/java/de/teamlapen/faction/client/integration/AppleSkinEventHandler.java
@@ -0,0 +1,45 @@
+package de.teamlapen.faction.client.integration;
+
+import de.teamlapen.faction.common.core.FactionDataComponents;
+import de.teamlapen.faction.common.world.items.consume.FactionFoodEntry;
+import de.teamlapen.faction.common.world.items.consume.FactionFoodList;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import squeek.appleskin.api.event.FoodValuesEvent;
+
+import java.util.List;
+
+@EventBusSubscriber
+public class AppleSkinEventHandler {
+
+    @SubscribeEvent
+    public static void applyFactionFoodValues(FoodValuesEvent event) {
+        ItemStack itemStack = event.itemStack;
+
+        if (itemStack.has(FactionDataComponents.FACTION_FOOD)) {
+            FactionFoodList foodList = itemStack.get(FactionDataComponents.FACTION_FOOD);
+            if (foodList == null) return;
+
+            List<FactionFoodEntry> entries = foodList.findMatchingEntries(event.player);
+            if (entries.isEmpty()) {
+                event.defaultFoodProperties = event.modifiedFoodProperties = foodList.defaultFood();
+                return;
+            }
+
+            int totalNutrition = 0;
+            float totalSaturationValue = 0;
+            for (FactionFoodEntry entry : entries) {
+                FoodProperties properties = entry.foodProperties();
+                totalNutrition += properties.nutrition();
+                totalSaturationValue += properties.saturation();
+            }
+            float predictiveSaturation = totalSaturationValue / (totalNutrition * 2F);
+
+            FoodProperties predictiveProperties = new FoodProperties.Builder().nutrition(totalNutrition).saturationModifier(predictiveSaturation).build();
+
+            event.defaultFoodProperties = event.modifiedFoodProperties = predictiveProperties;
+        }
+    }
+}
--- projects/faction/src/integrations/appleskin/java/de/teamlapen/faction/misc/mixin/FoodHelperMixin.java
+++ projects/faction/src/integrations/appleskin/java/de/teamlapen/faction/misc/mixin/FoodHelperMixin.java
@@ -1,21 +1,13 @@
 package de.teamlapen.faction.misc.mixin;
 
 import de.teamlapen.faction.common.core.FactionDataComponents;
-import de.teamlapen.faction.common.world.items.consume.FactionFoodEntry;
-import de.teamlapen.faction.common.world.items.consume.FactionFoodList;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.Consumable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import squeek.appleskin.helpers.ConsumableFood;
 import squeek.appleskin.helpers.FoodHelper;
-
-import java.util.List;
 
 @Mixin(FoodHelper.class)
 public class FoodHelperMixin {
@@ -23,33 +15,5 @@ public class FoodHelperMixin {
     @Inject(method = "isFood", at = @At("RETURN"), cancellable = true)
     private static void considerFactionFood(ItemStack itemStack, Player player, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(cir.getReturnValue() || itemStack.has(FactionDataComponents.FACTION_FOOD));
-    }
-
-    @Inject(method = "getDefaultFoodValues", at = @At("RETURN"), cancellable = true, remap = false)
-    private static void injectFactionFoodValues(ItemStack itemStack, Player player, CallbackInfoReturnable<ConsumableFood> cir) {
-        if (itemStack.has(FactionDataComponents.FACTION_FOOD)) {
-            FactionFoodList foodList = itemStack.get(FactionDataComponents.FACTION_FOOD);
-            if (foodList == null) return;
-
-            List<FactionFoodEntry> entries = foodList.findMatchingEntries(player);
-            if (entries.isEmpty()) {
-                cir.setReturnValue(new ConsumableFood(foodList.defaultFood(), itemStack.getOrDefault(DataComponents.CONSUMABLE, FoodHelper.DEFAULT_CONSUMABLE)));
-                return;
-            }
-
-            int totalNutrition = 0;
-            float totalSaturationValue = 0;
-            for (FactionFoodEntry entry : entries) {
-                FoodProperties properties = entry.foodProperties();
-                totalNutrition += properties.nutrition();
-                totalSaturationValue += properties.saturation();
-            }
-            float predictiveSaturation = totalSaturationValue / (totalNutrition * 2F);
-
-            FoodProperties predictiveProperties = new FoodProperties.Builder().nutrition(totalNutrition).saturationModifier(predictiveSaturation).build();
-
-            Consumable consumable = itemStack.getOrDefault(DataComponents.CONSUMABLE, FoodHelper.DEFAULT_CONSUMABLE);
-            cir.setReturnValue(new ConsumableFood(predictiveProperties, consumable));
-        }
     }
 }
