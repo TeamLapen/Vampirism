46fe6888ffa5861d4268dcd788bc444dbf336359
cheaterpaul <dev@paube.de>
2026-04-26 21:04:28 +0200

    fix guide book vampire blood formatting
    from 0f8d867af4dcc82280bd1337f0f605e689bf9d91

--- projects/vampirism/src/integrations/guide/java/de/teamlapen/vampirism/common/integration/guide/GuideBook.java
+++ projects/vampirism/src/integrations/guide/java/de/teamlapen/vampirism/common/integration/guide/GuideBook.java
@@ -448,7 +448,7 @@ public class GuideBook implements IGuideBook {
         helper.info(ModItems.VAMPIRE_FANG.get()).build(entries);
         helper.info(ModItems.HUMAN_HEART.get()).build(entries);
         helper.info(ModItems.PURE_BLOOD_0.get(), ModItems.PURE_BLOOD_1.get(), ModItems.PURE_BLOOD_2.get(), ModItems.PURE_BLOOD_3.get(), ModItems.PURE_BLOOD_4.get()).setFormats(translateComponent(ModEntities.VAMPIRE_BARON.get().getDescriptionId())).build(entries);
-        helper.info(ModItems.VAMPIRE_BLOOD_BOTTLE.get()).setFormats(translateComponent(ModEntities.VAMPIRE.get().getDescriptionId()), translateComponent(ModEntities.ADVANCED_VAMPIRE.get().getDescriptionId(), loc(ModItems.STAKE.get()))).build(entries);
+        helper.info(ModItems.VAMPIRE_BLOOD_BOTTLE.get()).setFormats(translateComponent(ModEntities.VAMPIRE.get().getDescriptionId()), translateComponent(ModEntities.ADVANCED_VAMPIRE.get().getDescriptionId()), loc(ModItems.STAKE.get())).build(entries);
         helper.info(ModItems.VAMPIRE_BOOK.get()).build(entries);
         helper.info(ModItems.OBLIVION_POTION.get()).customPages(new PageTaskItemStack(ModTasks.OBLIVION_POTION)).build(entries);
 
