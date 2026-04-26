c57842c95be0e32ed74da5ae91acbeaf02b00f00
cheaterpaul <dev@paube.de>
2026-04-26 20:32:23 +0200

    disable fog for spectator
    from cd53fc80be63a5efc82e9c842b20e60394ed2b72

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/client/renderer/RenderHandler.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/client/renderer/RenderHandler.java
@@ -79,7 +79,7 @@ public class RenderHandler implements IMinecraftAccessor {
         VampirePlayer vampire = VampirePlayer.get(player());
 
         //Vampire biome/village fog
-        if (player().tickCount % 10 == 0) {
+        if (!player().isSpectator() && player().tickCount % 10 == 0) {
             if ((ModConfig.client().renderVampireForestFog.get() || ModConfig.server().enforceRenderForestFog.get()) && (Helper.isEntityInArtificalVampireFogArea(player()) || Helper.isEntityInVampireBiome(player()))) {
                 insideFog = true;
                 vampireBiomeFogDistanceMultiplier = vampire.getLevel() > 0 ? 2 : 1;
