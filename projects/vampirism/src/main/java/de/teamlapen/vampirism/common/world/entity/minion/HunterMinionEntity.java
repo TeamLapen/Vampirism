070eb3da14e873f54c76ed75c4f5b908b70f6cfe
cheaterpaul <dev@paube.de>
2026-04-26 19:34:45 +0200

    Fix crash when a minion is trying to shoot with a tech crossbow. (The used list does not support iterator remove)
    from 60a670f208ab8b628998c88e23e1866f56f0f008

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/common/world/entity/minion/HunterMinionEntity.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/common/world/entity/minion/HunterMinionEntity.java
@@ -223,7 +223,7 @@ public class HunterMinionEntity extends MinionEntity<HunterMinionEntity.HunterMi
         if (stack.getItem() instanceof IHunterCrossbow) {
             if (stack.getItem() instanceof TechCrossbowItem) {
                 var clip = ModItems.ARROW_CLIP.get().getDefaultInstance();
-                ModItems.ARROW_CLIP.get().addArrows(clip, Collections.nCopies(12, ModItems.CROSSBOW_ARROW_NORMAL.get().getDefaultInstance()));
+                ModItems.ARROW_CLIP.get().addArrows(clip, Collections.nCopies(12, ModItems.CROSSBOW_ARROW_NORMAL.get().getDefaultInstance())); //Careful, all entries of the list are the same object, not copies
                 return clip;
             } else {
                 return ModItems.CROSSBOW_ARROW_NORMAL.get().getDefaultInstance();
