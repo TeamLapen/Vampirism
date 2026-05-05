c23400ec2df23d18c4054dd8efc0bdce07ff08e3
Piklach <50219644+Piklach@users.noreply.github.com>
2026-05-05 09:30:44 +0100

    Updates to GuideBook (#1589)
    * Update en_us.json
    
    Updated Syntax and Spelling Typos
    
    * Update en_us.json
    
    Updated Typos and syntax for Items Subsection
    
    * Update en_us.json
    
    Updated wording, syntax and spelling for the Vampirism Tab in the Guidebook
    
    * Update en_us.json
    
    Updated Syntax/formatting, Spelling errors and cleared up some language to be smoother -  Changelog, Blocks
    
    * Update en_us.json
    
    * Updated Language for Vampire Forest
    
    * Updated Dark Stone Entry to match names updated in 2023, block and recipes now show. Updated Text file to now show too under new title.
    Blocks have updated to new names where possible, unable to craft bloody dark stone brick. Unable to "craft" Dark Stone Brick and Dark Stone Tiles, unsure how to link to Minecraft's stonecutters recipes since the static is set to vampirism blocks and recipes.
    Polished Block, Stairs, Slabs and Walls Added to guidebook page
    
    * Updated recipe directory to now show garlic diffuser core and improved core in the guidebook
    
    * Added Garlic Finder Text Entry as referenced in guidebook but no text file made.
    Updated Hunter Lord Levels to Show "Title" Instead of "text.vampirism.title"
    Added Capitals to Hunter and Vampire Lord Headers on lord level pages
    
    * Update to show Swiftness Armour text file
    
    * Update to show Hunter Coat text file in guide
    
    * Updated guidebook to show requirement of 1x pureblood 1 (pureblood_0) for levels 6 and 7 then lowered the hunterleveling from 0 to -1 for level 5 so as to not show the reuirement of 0x pureblood 1
    
    * Updated purse salt's entry to correctly link recipe. Updated some last bits of Extra spaces, slight alterations to wording for some of the text files
    
    * Updated pure salt's entry to correctly link recipe. Updated some last bits of Extra spaces, slight alterations to wording for some of the text files
    
    * Fix garlic link
    
    ---------
    
    Co-authored-by: maxanier <dev@maxgb.de>

--- src/main/java/de/teamlapen/vampirism/entity/player/hunter/HunterLeveling.java
+++ src/main/java/de/teamlapen/vampirism/entity/player/hunter/HunterLeveling.java
@@ -15,7 +15,7 @@ public class HunterLeveling {
     private static final BasicHunterRequirement LEVEL_2 = new BasicHunterRequirement(2, 1);
     private static final BasicHunterRequirement LEVEL_3 = new BasicHunterRequirement(3, 5);
     private static final BasicHunterRequirement LEVEL_4 = new BasicHunterRequirement(4, 12);
-    private static final HunterTrainerRequirement LEVEL_5 = new HunterTrainerRequirement(5, 5, 0, new HunterTableRequirement(0, 10, 0, 0, 1, ModItems.HUNTER_INTEL_0));
+    private static final HunterTrainerRequirement LEVEL_5 = new HunterTrainerRequirement(5, 5, 0, new HunterTableRequirement(0, 10, 0, -1, 1, ModItems.HUNTER_INTEL_0));
     private static final HunterTrainerRequirement LEVEL_6 = new HunterTrainerRequirement(6, 10, 0, new HunterTableRequirement(0, 0, 1, 0, 1, ModItems.HUNTER_INTEL_1));
     private static final HunterTrainerRequirement LEVEL_7 = new HunterTrainerRequirement(7, 15, 0, new HunterTableRequirement(0, 10, 1, 0, 1, ModItems.HUNTER_INTEL_2));
     private static final HunterTrainerRequirement LEVEL_8 = new HunterTrainerRequirement(8, 40, 0, new HunterTableRequirement(1, 0, 1, 1, 1, ModItems.HUNTER_INTEL_3));
