2afb411f02bd0abc0b354703189cdc9716ea2b3e
maxanier <dev@maxgb.de>
2026-05-15 20:59:01 +0200

    Limit vapor still potion slots to 1 item, but accept any stacksize. Fixes 1597. Fix infinite brewing (potion is not altered but ingredient is consumed) Correct potion table slot assignment Comments and rename

--- projects/vampirism-api/src/main/java/de/teamlapen/vampirism/api/world/items/IExtendedBrewingRecipeRegistry.java
+++ projects/vampirism-api/src/main/java/de/teamlapen/vampirism/api/world/items/IExtendedBrewingRecipeRegistry.java
@@ -17,42 +17,64 @@ public interface IExtendedBrewingRecipeRegistry {
 
     boolean brewPotions(Level level, NonNullList<ItemStack> inputs, ItemStack ingredient, ItemStack extraIngredient, IExtendedBrewingCapabilities capabilities, int[] inputIndexes, boolean onlyExtended);
 
+    /**
+     *
+     * @param inputs       List with inputs at indices specified by inputIndexes
+     * @param inputIndexes Which positions of the inputs list should be tested
+     * @return Whether at least one of the given inputs has an output considering the ingredients
+     */
     boolean canBrew(Level level, NonNullList<ItemStack> inputs, ItemStack ingredient, ItemStack extraIngredient, IExtendedBrewingCapabilities capabilities, int[] inputIndexes);
 
     Optional<Triple<ItemStack, Integer, Integer>> getOutput(Level level, ItemStack bottle, ItemStack ingredient, ItemStack extraIngredient, IExtendedBrewingCapabilities capabilities, boolean onlyExtended);
 
     List<ExtendedPotionMix> getPotionMixes();
 
+    /**
+     * @return Whether the given input yields an output
+     */
     boolean hasOutput(Level level, ItemStack input, ItemStack ingredient, ItemStack extraIngredient, IExtendedBrewingCapabilities capabilities);
 
+    /**
+     * Ignores stack size
+     *
+     * @return Whether the given stack is a valid extra ingredient (Vampirism's extra ingredient) to any recipe.
+     */
     boolean isValidExtraIngredient(ItemStack stack);
 
+    /**
+     * Ignores stack size
+     * @return Whether the given stack is a valid ingredient (vanilla potion ingredient such as gunpowder) to any recipe.
+     */
     boolean isValidIngredient(PotionBrewing registry, ItemStack stack);
 
+    /**
+     * Ignores stack size
+     * @return Whether the given stack is a valid input (potion slot) (to any recipe).
+     */
     boolean isValidInput(PotionBrewing registry, ItemStack stack);
 
     interface IExtendedBrewingCapabilities {
-        default boolean isConcentratedBrewing() {
+        default boolean hasConcentratedBrewing() {
             return false;
         }
 
-        default boolean isDurableBrewing() {
+        default boolean hasDurableBrewing() {
             return false;
         }
 
-        default boolean isEfficientBrewing() {
+        default boolean hasEfficientBrewing() {
             return false;
         }
 
-        default boolean isMasterBrewing() {
+        default boolean hasMasterBrewing() {
             return false;
         }
 
-        default boolean isMultiTaskBrewing() {
+        default boolean hasMultiTaskBrewing() {
             return false;
         }
 
-        default boolean isSwiftBrewing() {
+        default boolean hasSwiftBrewing() {
             return false;
         }
     }
