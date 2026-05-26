ba9d0bab6f94d8ae0446641e2065d31b78d9fffa
TheGridExpert <grid.write.here@gmail.com>
2026-05-26 18:54:15 +0300

    Return the previous tombstones

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/common/util/VampirismVoxelShapes.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/common/util/VampirismVoxelShapes.java
@@ -40,13 +40,25 @@ public class VampirismVoxelShapes {
             Block.box(7, 4, 0, 9, 6, 2)
     ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).orElseGet(Shapes::empty);
 
-    public static final VoxelShape TOMBSTONE_SHORT = Shapes.join(Block.box(3, 2, 3, 13, 12, 7), Block.box(2, 0, 2, 14, 2, 8), BooleanOp.OR);
-    public static final VoxelShape TOMBSTONE_MEDIUM = Shapes.join(Block.box(2, 0, 2, 14, 2, 8), Block.box(3, 2, 3, 13, 16, 7), BooleanOp.OR);
+    public static final VoxelShape TOMBSTONE_SHORT = Stream.of(
+            Block.box(6, 10, 3, 10, 11, 6),
+            Block.box(2, 0, 2, 14, 1, 7),
+            Block.box(3, 1, 3, 13, 9, 6),
+            Block.box(4, 9, 3, 12, 10, 6)
+    ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+    public static final VoxelShape TOMBSTONE_MEDIUM = Stream.of(
+            Block.box(2, 0, 2, 14, 2, 7),
+            Block.box(3, 2, 3, 13, 14, 6),
+            Block.box(4, 14, 3, 12, 15, 6),
+            Block.box(6, 15, 3, 10, 16, 6)
+    ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
     public static final VoxelShape TOMBSTONE_CROSS = Stream.of(
-            Block.box(5, 23, 2, 11, 29, 8),
-            Block.box(2, 0, 1, 14, 2, 9),
-            Block.box(5, 2, 2, 11, 17, 8),
-            Block.box(0, 17, 2, 16, 23, 8)
+            Block.box(4, 20, 4, 12, 26, 8),
+            Block.box(5, 26, 4, 11, 27, 8),
+            Block.box(2, 0, 2, 14, 2, 10),
+            Block.box(4, 2, 4, 12, 14, 8),
+            Block.box(0, 14, 4, 16, 18, 8),
+            Block.box(5, 18, 4, 11, 20, 8)
     ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
     public static final VoxelShape TOMBSTONE_CROSS_BOTTOM = Shapes.join(TOMBSTONE_CROSS, Shapes.block(), BooleanOp.AND);
     public static final VoxelShape TOMBSTONE_CROSS_TOP = Shapes.join(TOMBSTONE_CROSS, Shapes.block().move(0, 1, 0), BooleanOp.AND).move(0, -1, 0);
