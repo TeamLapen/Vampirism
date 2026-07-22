14ad15ff0ca5649083cfb1b0fd08aeb26d40d93d
TheGridExpert <grid.write.here@gmail.com>
2026-07-22 13:54:35 +0300

    Remove a shift description tooltip from coffins, those have none

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/common/core/ModItems.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/common/core/ModItems.java
@@ -516,6 +516,6 @@ public class ModItems {
     }
 
     private static DeferredItem<CoffinItem> fromCoffin(DeferredHolder<Block, CoffinBlock> block) {
-        return fromBlock(block, (block1, itemProps) -> new CoffinItem((CoffinBlock) block1, itemProps.factions$withShiftDescription().rarity(Rarity.RARE).stacksTo(1).useBlockDescriptionPrefix()));
+        return fromBlock(block, (block1, itemProps) -> new CoffinItem((CoffinBlock) block1, itemProps.rarity(Rarity.RARE).stacksTo(1).useBlockDescriptionPrefix()));
     }
 }
