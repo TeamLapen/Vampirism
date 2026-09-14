e7b55e1c00524c2012d41ebcf700c7d76238157d
lunofe <git@lunofe.com>
2026-09-14 19:35:53 +0200

    protocol version is actually only 2

--- projects/vampirism/src/main/java/de/teamlapen/vampirism/common/network/ModPacketDispatcher.java
+++ projects/vampirism/src/main/java/de/teamlapen/vampirism/common/network/ModPacketDispatcher.java
@@ -10,7 +10,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public class ModPacketDispatcher {
 
-    private static final String PROTOCOL_VERSION = Integer.toString(4);
+    private static final String PROTOCOL_VERSION = Integer.toString(2);
 
     public static void registerHandler(RegisterPayloadHandlersEvent event) {
         registerPackets(event.registrar(REFERENCE.MODID).versioned(PROTOCOL_VERSION));
